6b361996328af1048919c63d6b457030c4d15edc
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-06-28 10:46:27 -0400

    fs.writeFile incorrect format

--- node-api/app.js
+++ node-api/app.js
@@ -1,5 +1,5 @@
-var fs = require("fs");
-var fileContent = "hello";
+const fs = require("fs");
+const fileContent = "hello";
 
 fs.writeFile("./sample.txt", fileContent, (err) => {
     if (err) {
@@ -10,3 +10,22 @@ fs.writeFile("./sample.txt", fileContent, (err) => {
 });
 
 // Content of the file -> hello
+
+const sampleObject = {
+    a: 1,
+    b: 2,
+    c: {
+        x: 11,
+        y: 22
+    }
+};
+
+fs.writeFile("./object.json", JSON.stringify(sampleObject), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    };
+    console.log("File has been created");
+});
+
+// Content of the file -> {"a":1,"b":2,"c":{"x":11,"y":22}}
