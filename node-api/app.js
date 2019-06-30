a06efbf665eeb67181559a1cb0730accec4cbe6d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-06-30 16:12:38 -0400

    fs.writeFile correctly beautified format

--- node-api/app.js
+++ node-api/app.js
@@ -29,3 +29,23 @@ fs.writeFile("./object.json", JSON.stringify(sampleObject), (err) => {
 });
 
 // Content of the file -> {"a":1,"b":2,"c":{"x":11,"y":22}}
+
+fs.writeFile("./object.json", JSON.stringify(sampleObject, null, 4), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    };
+    console.log("File has been created");
+});
+
+/*
+ Content of the file:
+ {
+     a: 1,
+     b: 2,
+     c: {
+         x: 11,
+         y: 22
+     }
+ }
+ */
