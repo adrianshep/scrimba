dab847bb7a995f47fe5452fc71e2661d36c7f29f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-07-30 08:57:51 -0400

    part 5 - simple web app - handlers

--- golang-practical/part5/main.go
+++ golang-practical/part5/main.go
@@ -1,9 +1,20 @@
 package main
 
-import ("fmt",
+import ("fmt"
         "net/http")
+
+// * means reading through http.Request, not its memory address
+func index_handler(w http.ResponseWriter, r *http.Request) {
+  // F will format based on what you specify, in this case, the writer itself, as well as output what you ask it to
+  fmt.Fprintf(w, "Whoa, Go is neat!")
+}
+
+func about_handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Expert web design by Sentdex")
+}
 
 func main() {
   http.HandleFunc("/", index_handler)
+  http.HandleFunc("/about/", about_handler)
   http.ListenAndServe(":8000", nil)
 }
