f9de62b00298e18ff73d4917a5fb0a21ad949e47
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-08-04 17:12:09 -0400

    part 9 - more web dev basics - html elements

--- golang-practical/part9-webdev-basics/main.go
+++ golang-practical/part9-webdev-basics/main.go
@@ -4,10 +4,19 @@ import ("fmt"
         "net/http")
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hey there")
+
+  fmt.Fprintf(w,`<h1>Hey there</h1>
+<p>Go is fast!</p>
+<p>...and simple!</p>`)
+
+  fmt.Fprintf(w,"<h1>Hey there</h1>")
+  fmt.Fprintf(w,"<p>Go is fast!</p>")
+  fmt.Fprintf(w,"<p>...and simple!</p>")
+  fmt.Fprintf(w,"<p>You %s even add %s</p>","can","<strong>variables</strong>")
 }
 
 func main() {
   http.HandleFunc("/", index_handler)
   http.ListenAndServe(":8000", nil)
+  // for https, http.ListenAndServeTLS
 }
