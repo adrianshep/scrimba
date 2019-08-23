36c1435e6736ecb2560c0bd99072334a85440ce1
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-08-23 09:01:57 -0400

    part 17 - using templates - newsaggtemplate

--- golang-practical/part17-using-templates/main.go
+++ golang-practical/part17-using-templates/main.go
@@ -29,9 +29,28 @@ type NewsAggPage struct {
 }
 
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-  p := NewsAggPage{Title: "Amazing News Aggregator", News: "some news"}
-  t, _ := template.ParseFiles("basictemplating.html")
-  t.Execute(w,p)
+  var s Sitemapindex
+  var n News
+  // map where key is a string and NewsMap will be our values
+  news_map := make(map[string]NewsMap)
+  resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+  bytes, _ := ioutil.ReadAll(resp.Body)
+  xml.Unmarshal(bytes, &s)
+
+  for _, Location, := range s.Locations {
+    resp, _ := http.Get(Location)
+    bytes, _ := ioutil.ReadAll(resp.Body)
+    // as we iterate over this information, we can store it into our map
+    xml.Unmarshal(bytes, &n)
+    // idx is index:
+    for idx, _ := range n.Titles{
+      news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx]}, n.Locations[idx]
+    }
+  }
+
+  p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+  t, _ := template.ParseFiles("newsaggtemplate.html")
+  fmt.Fprintln(t.Execute(w, p))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
