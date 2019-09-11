0742aa8412941f138f887dea21b72cb3e3247a0f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-09-11 12:04:23 -0400

    2-JS-basics - variable mutation and type coercion

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -1,3 +1,7 @@
+/****************************** 
+Variables and data types
+*/
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -13,6 +17,36 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
-// $ or _ are only non-alpha characters allowed to start variable names
+
+// Variable naming rules
+// $ or _ are only non-alpha characters allowed to start variable names:
 var $3years = 3;
 var _3years = 3;
+*/
+
+/******************************
+ * Variable mutation and type coercion
+ */
+
+ var firstName = 'John';
+ var age = 28;
+
+//  Type coercion
+ console.log(firstName + ' ' + age);
+
+ var job, isMarried;
+ job = 'teacher';
+//  isMarried = false;
+
+// commented out isMarried will throw an undefined that is then coerced into a string along with other elements:
+ console.log(firstName + ' is a ' + age + ' year old ' + job + ', Is he married? ' + isMarried);
+
+//  Variable mutation
+age = 'twenty-eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' yeard old ' + job + ', Is he married? ' + isMarried);
+
+// prompt() receives input from user which is then assigned to lastName:
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
