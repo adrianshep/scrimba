f00363811398c73da3eab459dfc3016f0e40e2cc
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-09-27 12:27:43 -0400

    2-JS-basics - coding challenge 3 - alternate solution

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge3.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge3.js
@@ -5,11 +5,11 @@ var billsWithTips = [];
 
 function tipCalculator(bill) {
     if (bill < 50) {
-        tip = 0.20
+        tip = 0.20;
     } else if (bill >= 50 && bill < 200) {
-        tip = 0.15
+        tip = 0.15;
     } else {
-        tip = 0.10
+        tip = 0.10;
     }
     var billTip = bill * tip;
     tips.push(billTip);
@@ -21,3 +21,18 @@ function tipCalculator(bill) {
 tipCalculator(124);
 tipCalculator(48);
 tipCalculator(268);
+
+/*
+Alternate solution:
+
+var bills = [124, 48, 268];
+tips = [tipCalculator(bills[0]),
+        tipCalculator(bills[1]),
+        tipCalculator(bills[2])];
+
+billsWithTips = [bills[0] + tips[0],
+                 bills[1] + tips[1],
+                 bills[2] + tips[2]];
+
+console.log(tips, billsWithTips);
+*/
