17b27545137383a49c6e9dbc23d7b8ac5348226e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-09-28 13:08:18 -0400

    2-JS-basics - objects and properties

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -319,7 +319,7 @@ console.log(whatDoYouDo('retired', 'Mark'));
 /******************************
 * Arrays
 */
-
+/*
 // Initialize new array
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1969, 1948);
@@ -356,3 +356,34 @@ console.log(john.indexOf(23));
 
 var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
 console.log(isDesigner);
+*/
+
+/******************************
+* Objects and properties
+*/
+
+// Object literal
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+// new Object syntax
+var jane = new Object();
+jane.name = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
