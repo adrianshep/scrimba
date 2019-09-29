0313cfbbf52bc7a00c82e4205ec23c3cf4e94b28
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-09-29 16:00:59 -0400

    2-JS-basics - objects and methods

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -361,7 +361,7 @@ console.log(isDesigner);
 /******************************
 * Objects and properties
 */
-
+/*
 // Object literal
 var john = {
     firstName: 'John',
@@ -387,3 +387,26 @@ jane.name = 'Jane';
 jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
+*/
+
+/******************************
+* Objects and methods
+*/
+/*
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        // "this" means this object, the present, current object:
+        this.age = 2018 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
+*/
+
