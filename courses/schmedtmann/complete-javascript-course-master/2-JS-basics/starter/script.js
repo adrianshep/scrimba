6d378e276f01d35b4fd6ac1b8b606488acc9e78f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-10-02 13:26:58 -0400

    2-JS-basics - loops and iteration

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -410,3 +410,46 @@ john.calcAge();
 console.log(john);
 */
 
+/******************************
+* Loops and iteration
+*/
+/*
+// For loop
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+} 
+
+for (var i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
+*/
+/*
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
+
+// While loop
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+
+// Continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+}
