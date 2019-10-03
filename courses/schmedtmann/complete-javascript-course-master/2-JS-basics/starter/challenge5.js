ce6b0acb3eec14eee5f3ef85c50a07eb0ee217c7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-10-03 11:56:52 -0400

    2-JS-basics - coding challenge 5 - setup

---
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge5.js
@@ -0,0 +1,16 @@
+var johnTipCalculator = {
+    bills: [124, 48, 268, 180, 42],
+    tips: [],
+    billTips: [],
+    calcTip: function() {
+        
+        /* if (bills[i] < 50) {
+            bills[i] * 0.20;
+        } else if (bills[i] >= 50 && bills[i] < 200) {
+            bills[i] * 0.15;
+        } else {
+            bills[i] * 0.10;
+        }
+        */
+    }
+};
