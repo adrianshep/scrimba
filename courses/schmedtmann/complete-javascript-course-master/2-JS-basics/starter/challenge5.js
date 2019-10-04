2e2e3ea3b5f4480448845e2373eca4e2b676e775
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-10-04 17:07:29 -0400

    2-JS-basics - coding challenge 5 - add thises

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge5.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge5.js
@@ -2,18 +2,21 @@ var johnTipCalculator = {
     bills: [124, 48, 268, 180, 42],
     tips: [],
     billTips: [],
-    calcTip: function() {
-        for (var i = 0; i < bills.length - 1; i++) {
-            if (bills[i] < 50) {
-                tips.push(bills[i] * 0.20);
-                billTips.push(bills[i] + tips[i]);
-            } else if (bills[i] >= 50 && bills[i] < 200) {
-                tips.push(bills[i] * 0.15);
-                billTips.push(bills[i] + tips[i]);
+    calcTips: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            if (this.bills[i] < 50) {
+                this.tips.push(this.bills[i] * 0.20);
+                this.billTips.push(this.bills[i] + this.tips[i]);
+            } else if (this.bills[i] >= 50 && this.bills[i] < 200) {
+                this.tips.push(this.bills[i] * 0.15);
+                this.billTips.push(this.bills[i] + this.tips[i]);
             } else {
-                tips.push(bills[i] * 0.10);
-                billTips.push(bills[i] + tips[i]);
+                this.tips.push(this.bills[i] * 0.10);
+                this.billTips.push(this.bills[i] + this.tips[i]);
             }
         };
     }
 };
+
+johnTipCalculator.calcTips();
+console.log(johnTipCalculator.tips, johnTipCalculator.billTips);
