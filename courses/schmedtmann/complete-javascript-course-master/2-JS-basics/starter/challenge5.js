04f64d1b9a486b12746d06a982cde543d17070d9
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-10-05 11:51:50 -0400

    2-JS-basics - coding challenge 5 - calcAveTip

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge5.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge5.js
@@ -1,23 +1,3 @@
-var markTipCalculator = {
-    bills: [77, 375, 110, 45],
-    tips: [],
-    billTips: [],
-    calcTips: function() {
-        for (var i = 0; i < this.bills.length; i++) {
-            if (this.bills[i] < 100) {
-                this.tips.push(this.bills[i] * 0.20);
-                this.billTips.push(this.bills[i] + this.tips[i]);
-            } else if (this.bills[i] >= 100 && this.bills[i] < 300) {
-                this.tips.push(this.bills[i] * 0.10);
-                this.billTips.push(this.bills[i] + this.tips[i]);
-            } else {
-                this.tips.push(this.bills[i] * 0.25);
-                this.billTips.push(this.bills[i] + this.tips[i]);
-            }
-        };
-    }
-};
-
 var johnTipCalculator = {
     bills: [124, 48, 268, 180, 42],
     tips: [],
@@ -38,5 +18,40 @@ var johnTipCalculator = {
     }
 };
 
+var markTipCalculator = {
+    bills: [77, 375, 110, 45],
+    tips: [],
+    billTips: [],
+    calcTips: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            if (this.bills[i] < 100) {
+                this.tips.push(this.bills[i] * 0.20);
+                this.billTips.push(this.bills[i] + this.tips[i]);
+            } else if (this.bills[i] >= 100 && this.bills[i] < 300) {
+                this.tips.push(this.bills[i] * 0.10);
+                this.billTips.push(this.bills[i] + this.tips[i]);
+            } else {
+                this.tips.push(this.bills[i] * 0.25);
+                this.billTips.push(this.bills[i] + this.tips[i]);
+            }
+        };
+    }
+};
+
+function calcAveTip(tips) {
+    for (var i = 0; i < tips.length; i++) {
+        var totalTips = tips[i] + totalTips;
+    }
+    var aveTips = totalTips / tips.length;
+    return aveTips;
+}
+
+function highestTippingFamily(aveTips) {
+    
+}
+
 johnTipCalculator.calcTips();
 console.log(johnTipCalculator.tips, johnTipCalculator.billTips);
+
+markTipCalculator.calcTips();
+console.log(markTipCalculator.tips, markTipCalculator.billTips);
