7d6b76fc31d9d5950b535b1caf5838ba29d8ca27
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-10-07 12:56:57 -0400

    2-JS-basics - coding challenge 5 - Jonas's solution

--- courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge5.js
+++ courses/schmedtmann/complete-javascript-course-master/2-JS-basics/starter/challenge5.js
@@ -1,39 +1,49 @@
-var johnTipCalculator = {
+var john = {
+    fullName: 'John Smith',
     bills: [124, 48, 268, 180, 42],
-    tips: [],
-    billTips: [],
     calcTips: function() {
+        this.tips = [];
+        this.billsPlusTips = [];
+        
         for (var i = 0; i < this.bills.length; i++) {
-            if (this.bills[i] < 50) {
-                this.tips.push(this.bills[i] * 0.20);
-                this.billTips.push(this.bills[i] + this.tips[i]);
-            } else if (this.bills[i] >= 50 && this.bills[i] < 200) {
-                this.tips.push(this.bills[i] * 0.15);
-                this.billTips.push(this.bills[i] + this.tips[i]);
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 50) {
+                percentage = 0.20;
+            } else if (bill >= 50 && bill < 200) {
+                percentage = 0.15;
             } else {
-                this.tips.push(this.bills[i] * 0.10);
-                this.billTips.push(this.bills[i] + this.tips[i]);
+                percentage = 0.10;
             }
+
+            this.tips[i] = bill * percentage;
+            this.billsPlusTips[i] = bill + bill * percentage;
         };
     }
 };
 
-var markTipCalculator = {
+var mark = {
+    fullName: 'Mark Miller',
     bills: [77, 375, 110, 45],
-    tips: [],
-    billTips: [],
     calcTips: function() {
+        this.tips = [];
+        this.billsPlusTips = [];
+        
         for (var i = 0; i < this.bills.length; i++) {
-            if (this.bills[i] < 100) {
-                this.tips.push(this.bills[i] * 0.20);
-                this.billTips.push(this.bills[i] + this.tips[i]);
-            } else if (this.bills[i] >= 100 && this.bills[i] < 300) {
-                this.tips.push(this.bills[i] * 0.10);
-                this.billTips.push(this.bills[i] + this.tips[i]);
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 100) {
+                percentage = 0.20;
+            } else if (bill >= 100 && bill < 300) {
+                percentage = 0.10;
             } else {
-                this.tips.push(this.bills[i] * 0.25);
-                this.billTips.push(this.bills[i] + this.tips[i]);
+                percentage = 0.25;
             }
+
+            this.tips[i] = bill * percentage;
+            this.billsPlusTips[i] = bill + bill * percentage;
         };
     }
 };
@@ -41,25 +51,22 @@ var markTipCalculator = {
 function calcAveTip(tips) {
     var totalTips = 0;
     for (var i = 0; i < tips.length; i++) {
-        totalTips = tips[i] + totalTips;
+        totalTips = totalTips + tips[i];
     }
-    var aveTips = totalTips / tips.length;
-    return aveTips;
+    return totalTips / tips.length;
 }
 
-johnTipCalculator.calcTips();
-console.log(johnTipCalculator.tips, johnTipCalculator.billTips);
+john.calcTips();
+mark.calcTips();
 
-markTipCalculator.calcTips();
-console.log(markTipCalculator.tips, markTipCalculator.billTips);
+john.average = calcAveTip(john.tips);
+mark.average = calcAveTip(mark.tips);
+console.log(john, mark);
 
-var johnAveTip = calcAveTip(johnTipCalculator.tips);
-var markAveTip = calcAveTip(markTipCalculator.tips);
-
-if (johnAveTip > markAveTip) {
-    console.log("John's family's average tip of " + johnAveTip + " is higher than Mark's family's average tip of " + markAveTip + ".");
-} else if (markAveTip > johnAveTip) {
-    console.log("Mark's family's average tip of " + markAveTip + " is higher than John's family's average tip of " + johnAveTip + ".");
+if (john.average > mark.average) {
+    console.log(john.fullName + "'s family's average tip of " + john.average + " is higher than " + mark.fullName + "'s family's average tip of " + mark.average + ".");
+} else if (mark.average > john.average) {
+    console.log(mark.fullName + "'s family's average tip of " + mark.average + " is higher than " + john.fullName + "'s family's average tip of " + john.average + ".");
 } else {
-    console.log("John's family's average tip of " + johnAveTip + " and Mark's family's average tip of " + markAveTip + " are equal.");
+    console.log(john.fullName + "'s family's average tip of " + john.average + " and " + mark.fullName + "'s family's average tip of " + mark.average + " are equal.");
 }
