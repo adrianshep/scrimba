bff5142e918f8998e4a79264066a8b432ae9144b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-11-07 12:55:12 -0500

    5-advanced-JS - closures

--- courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -183,7 +183,7 @@ function game() {
 }
 game();
 */
-
+/*
 (function () {
     var score = Math.random() * 10;
     console.log(score >= 5);
@@ -196,7 +196,44 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
 })(5);
+*/
 
 
+////////////////////////////////////
+// Lecture: Closures
 
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
 
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+// retirement(66)(1990);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ' , what do you do?');
+        }
+    }
+}
+*/
