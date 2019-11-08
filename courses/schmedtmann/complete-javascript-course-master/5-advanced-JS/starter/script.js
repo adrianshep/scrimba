330f0bef11489bce68fb5036dadee44ad48da967
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-11-08 13:17:35 -0500

    5-advanced-JS - closures - challenge - Jonas's solution

--- courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -220,35 +220,6 @@ retirementIceland(1990);
 
 // retirement(66)(1990);
 */
-
-function interviewQuestion(job) {
-    return function(question, name) {
-        console.log(question);
-    }
-}
-
-var designerQuestion = interviewQuestion('designer')(name + ', can you please explain what UX design is?');
-
-designerQuestion('designer', 'Mark');
-
-/*
-var designerQuestion = interviewQuestion('designer')(function(name) {
-    question = name + ', can you please explain what UX design is?';
-    return question;
-})();
-var teacherQuestion = interviewQuestion('teacher')(function(name) {
-    question = 'What subject do you teach, ' + name + '?';
-    return question;
-})();
-var unknownQuestion = interviewQuestion()(function(name) {
-    question = 'Hello ' + name + ' , what do you do?';
-    return question;
-})();
-*/
-
-
-
-
 /*
 function interviewQuestion(job) {
     if (job === 'designer') {
@@ -266,3 +237,27 @@ function interviewQuestion(job) {
     }
 }
 */
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello, ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
+interviewQuestion('longshoreman')('Biff');
