4a55400bb2bec987b486d1cd6c770ae259c379da
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-11-09 20:11:37 -0500

    5-advanced-JS - bind, call and apply

--- courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -237,7 +237,7 @@ function interviewQuestion(job) {
     }
 }
 */
-
+/*
 function interviewQuestion(job) {
     return function(name) {
         if (job === 'designer') {
@@ -261,3 +261,74 @@ designerQuestion('Mike');
 
 interviewQuestion('teacher')('Mark');
 interviewQuestion('longshoreman')('Biff');
+*/
+
+
+
+
+
+
+
+////////////////////////////////////
+// Lecture: Bind, call and apply
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', ladies and gentlemen! I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+// the apply method
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
