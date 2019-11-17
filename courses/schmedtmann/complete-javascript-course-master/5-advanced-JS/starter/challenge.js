9a538e2de3f12e7e0f833841d76e78778d07459e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-11-16 20:54:39 -0500

    5-advanced-JS - coding challenge 7 - expert level challenge

--- courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/challenge.js
+++ courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/challenge.js
@@ -52,9 +52,21 @@ Question.prototype.answerPrompt = (function() {
     if (user == questions[random].correct) {
         console.log("You are correct!", "user's answer: ", user, "correct answer:", questions[random].correct);
     } else {
-        console.log("Sorry, that ain't it!", "user's answer: ", user, "correct answer:", questions[random].correct);
+        console.log("Sorry, that ain't it!", "user's answer:", user, ", correct answer:", questions[random].correct);
         }
 })();
 
 // asteroid.questionSelect();
 // asteroid.answerPrompt();
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
