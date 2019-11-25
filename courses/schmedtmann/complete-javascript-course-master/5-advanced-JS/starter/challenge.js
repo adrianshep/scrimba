38e91ea5c008c17244c8461b1e1a9a57ba34201c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-11-24 19:39:00 -0500

    5-advanced-JS - coding challenge 7 - Jonas's solution - part 2 expert level - score closure and keepScore

--- courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/challenge.js
+++ courses/schmedtmann/complete-javascript-course-master/5-advanced-JS/starter/challenge.js
@@ -166,14 +166,24 @@ Question.prototype.answerCheck = (function() {
         }
     }
     
-    Question.prototype.checkAnswer = function(ans) {
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
         if (ans === this.correct) {
             console.log('Correct answer!');
+            sc = callback(true);
         } else {
             console.log('Wrong answer. Try again: ')
+
+            sc = callback(false);
         }
+        this.displayScore(sc);
     }
     
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('------------------');
+    }
+
     var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
     
     var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
@@ -181,6 +191,18 @@ Question.prototype.answerCheck = (function() {
     var q3 = new Question('What best describes coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
 
     var questions = [q1, q2, q3];
+
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+
+    var keepScore = score();
     
     function nextQuestion() {
     
@@ -192,7 +214,7 @@ Question.prototype.answerCheck = (function() {
 
         if (answer !== 'exit') {
 
-            questions[n].checkAnswer(parseInt(answer));
+            questions[n].checkAnswer(parseInt(answer), keepScore);
 
             nextQuestion();
         }
