66e7abb6c22b10b44c8c8b470323b95705861eed
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-12-14 13:03:17 -0500

    6-budgety-JS - finishing touches: improving the ux

--- courses/schmedtmann/complete-javascript-course-master/6-budgety/starter/app.js
+++ courses/schmedtmann/complete-javascript-course-master/6-budgety/starter/app.js
@@ -189,6 +189,12 @@ var UIController = (function() {
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getinput: function() {
             return {
@@ -263,12 +269,6 @@ var UIController = (function() {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
 
                 if (percentages[index] > 0) {
@@ -291,10 +291,23 @@ var UIController = (function() {
 
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
-
         },
 
-        getDOMStrings: function() {
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+        },
+
+        getDOMstrings: function() {
             return DOMstrings;
         }
     };
@@ -308,7 +321,7 @@ var UIController = (function() {
 var controller = (function(budgetCtrl, UICtrl) {
 
     var setupEventListeners = function() {
-        var DOM = UICtrl.getDOMStrings();
+        var DOM = UICtrl.getDOMstrings();
         
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
@@ -319,6 +332,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
     };
 
