38ebf74a116cf58624b6849b2bb51b08933030f0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-12-31 16:30:38 -0500

    7-ES6 - coding challenge 8 - create Street classifyStreetSize method

--- courses/schmedtmann/complete-javascript-course-master/7-ES6/starter/challenge.js
+++ courses/schmedtmann/complete-javascript-course-master/7-ES6/starter/challenge.js
@@ -48,20 +48,43 @@ class Street extends TownElement {
         this.streetLength = streetLength;
         this.streetSize = streetSize;
     }
+
+    classifyStreetSize(streetLength) {
+        if ((this.streetLength) <= 50) {
+            this.streetSize = 'tiny';
+        } else if ((this.streetLength) <= 100) {
+            this.streetSize = 'small';
+        } else if ((this.streetLength) <= 200) {
+            this.streetSize = 'normal';
+        } else if ((this.streetLength) <= 400) {
+            this.streetSize = 'big';
+        } else if ((this.streetLength) > 400) {
+            this.streetSize = 'huge';
+        } else {
+            this.streetSize = 'normal';
+        }
+        console.log(`Size classification for ${this.name} is: ${this.streetSize}`);
+    }
 }
 
-const geneseehillsPark = new Park('Genesee Hills Park', '1957', 69, 10000);
+const geneseehillsPark = new Park('Genesee Hills Park', '1957', 1100, 10000);
 
-const mosesdewittPark = new Park('Moses DeWitt Park', '1931', 75, 8500);
+const mosesdewittPark = new Park('Moses DeWitt Park', '1931', 750, 8500);
 
-const eriecanalPark = new Park('Erie Canal Park', '1960', 42, 4000);
+const eriecanalPark = new Park('Erie Canal Park', '1960', 420, 4000);
 
-const briarcliffRd = new Street('Briarcliff Road', 1940, 200, 'normal');
+const briarcliffRd = new Street('Briarcliff Road', 1940, 200);
 
-const radcliffeRd = new Street('Radcliffe Road', 1935, 400, 'big');
+const radcliffeRd = new Street('Radcliffe Road', 1935, 400);
 
-const salemRd = new Street('Salem Road', 1936, 30, 'tiny');
+const salemRd = new Street('Salem Road', 1936, 30);
 
-const revereRd = new Street('Revere Road', 1938, 300, 'normal');
+const revereRd = new Street('Revere Road', 1938, 300);
+
+const falstaffRd = new Street('Falstaff Road', 1938);
 
 geneseehillsPark.calcTreeDensity();
+
+salemRd.classifyStreetSize();
+
+falstaffRd.classifyStreetSize();
