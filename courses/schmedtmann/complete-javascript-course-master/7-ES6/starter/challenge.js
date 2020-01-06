72317e20815e78d11eea24d39a22aae331cf2308
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-01-06 12:27:33 -0500

    7-ES6 - coding challenge 8 - Jonas's solution - Street subclass and classifyStreet method

--- courses/schmedtmann/complete-javascript-course-master/7-ES6/starter/challenge.js
+++ courses/schmedtmann/complete-javascript-course-master/7-ES6/starter/challenge.js
@@ -147,3 +147,21 @@ class Park extends Element {
     }
 }
 
+
+class Street extends Element {
+    constructor(name, buildYear, length, size = 3) {
+        super(name, buildYear);
+        this.length = length;
+        this.size = size;
+    }
+
+    classifyStreet {
+        const classification = new Map();
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'normal');
+        classification.set(4, 'big');
+        classification.set(5, 'huge');
+        console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)}`);
+    }
+}
