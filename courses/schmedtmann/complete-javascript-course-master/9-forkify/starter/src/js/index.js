0116fe740b0d18fee469d944013be28ae40962eb
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-01-24 13:19:21 -0500

    9-Forkify - build the search model

--- courses/schmedtmann/complete-javascript-course-master/9-forkify/starter/src/js/index.js
+++ courses/schmedtmann/complete-javascript-course-master/9-forkify/starter/src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    // const key = 'API key would go here',
-    try {
-        const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
