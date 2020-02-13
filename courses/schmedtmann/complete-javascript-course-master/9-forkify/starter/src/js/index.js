ad298449f3061705f20f5ee0d90d2cf3a6b5c770
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-02-13 12:41:03 -0500

    9-Forkify - final considerations - remove window state leads and console logs

--- courses/schmedtmann/complete-javascript-course-master/9-forkify/starter/src/js/index.js
+++ courses/schmedtmann/complete-javascript-course-master/9-forkify/starter/src/js/index.js
@@ -15,7 +15,6 @@ import { elements, renderLoader, clearLoader } from './views/base';
  * - Liked recipes
  */
 const state = {};
-window.state = state;
 
 /**
 * SEARCH CONTROLLER
@@ -69,7 +68,6 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
     // Get ID from url
     const id = window.location.hash.replace('#', '');
-    console.log(id);
 
     if (id) {
         // Prepare UI for changes
@@ -144,7 +142,6 @@ elements.shopping.addEventListener('click', e => {
 /**
 * LIKE CONTROLLER
 */
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -213,5 +210,3 @@ elements.recipe.addEventListener('click', e => {
         controlLike();
     }
 });
-
-window.l = new List();
