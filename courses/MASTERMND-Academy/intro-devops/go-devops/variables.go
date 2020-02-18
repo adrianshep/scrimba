a65d0952a3e80e859f33ddc7d7ff1ddd32ec670d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-02-18 18:14:30 -0500

    intro to devops - intro to programming with go pt 1 - variables

---
+++ courses/MASTERMND-Academy/intro-devops/go-devops/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variable declaration:
+	// var name string
+	// can only declare variable a single time
+
+	// variable assignment:
+	// name = "Ado"
+
+	// variable declaration and assignment in one step:
+	// var name string = "Adrian"
+	// shorthand for variable declaration and assignment:
+	name := "Ado"
+
+	fmt.Println(name)
+}
