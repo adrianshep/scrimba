363b62d950a34f22313b580316326f28971c33ff
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-02-19 22:39:41 -0500

    intro to devops - intro to programming with go pt 1 - data types - zero values

---
+++ courses/MASTERMND-Academy/intro-devops/go-devops/data_types.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// zero values: variables declared without an excplicit initial value are given zero values
+	// var i int
+	// var f float64
+	// var b bool
+	// var s string
+	// fmt.Printf("%v %v %v %q\n", i, f, b, s)
+
+	// zero values are:
+	// 0 for numeric types,
+	// false for boolean type,
+	// "" empty string for strings
+
+	// name := "Ado"
+	// age := 29
+
+	fmt.Print(5 + 5)
+	// has to be Print, not Printf which only formats strings
+
+	// type coercion or type casting
+	fmt.Printf(string(5 + 5))
+}
