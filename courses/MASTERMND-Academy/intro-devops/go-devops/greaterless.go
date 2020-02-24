480199e2aaa2cabe5d82f93f57c242e62224a34d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-02-23 20:55:08 -0500

    intro to devops - intro to programming with go pt 2 - comparison operators - greater than less than

---
+++ courses/MASTERMND-Academy/intro-devops/go-devops/greaterless.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(2000 > 5)
+	fmt.Println(2000 < 5)
+	fmt.Println(2000 >= 5)
+	fmt.Println(2000 <= 5)
+	fmt.Println("this" == "That")
+	fmt.Println("this" == "this")
+}
