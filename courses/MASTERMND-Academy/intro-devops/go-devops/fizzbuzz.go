41a56c6475e994988de540a6c9d3f0bdd53f4450
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-02-28 10:58:55 -0500

    intro to devops - intro to programming with go pt 2 - fizzbuzz - print fizzbuzz

--- courses/MASTERMND-Academy/intro-devops/go-devops/fizzbuzz.go
+++ courses/MASTERMND-Academy/intro-devops/go-devops/fizzbuzz.go
@@ -9,6 +9,10 @@ import "fmt"
 
 func main() {
 	for num := 0; num <= 100; num++ {
-		fmt.Println(num)
+		if num%3 == 0 && num%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else {
+			fmt.Println(num)
+		}
 	}
 }
