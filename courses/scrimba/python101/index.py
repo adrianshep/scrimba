9350cc12c21518b1c9e444ff20c98f2873fa8208
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-03-13 13:39:12 -0400

    variables

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -1,3 +1,18 @@
-print("Create hammer")
-print("Create nails")
-print("Use hammer and nails")
+
+# Print Statement and Program Flow
+
+# print("Create hammer")
+# print("Create nails")
+# print("Use hammer and nails")
+
+##############################
+
+# Variables
+
+failed_subjects="2"
+name='John'
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
+print(name + '  will need to redo ' + failed_subjects + '  courses.')
+name="Eric"
+print(name + '  is doing well in geography.')
