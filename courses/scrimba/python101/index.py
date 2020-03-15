057326c4e16d130562e7bf3db3697efea01f917f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-03-14 22:48:36 -0400

    data types and type casting

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -1,9 +1,9 @@
 
 # Print Statement and Program Flow
 
-# print("Create hammer")
-# print("Create nails")
-# print("Use hammer and nails")
+print("Create hammer")
+print("Create nails")
+print("Use hammer and nails")
 
 ##############################
 
@@ -16,3 +16,21 @@ print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
 print(name + '  will need to redo ' + failed_subjects + '  courses.')
 name="Eric"
 print(name + '  is doing well in geography.')
+
+##############################
+
+# Data Types and Type Casting
+
+a = int(1)        # a will be 1
+b = int(2.5)      # b will be 2
+c = int("3")      # c will be 3
+c1 = int(float("3.4"))   # c1 will be...
+d = float(1)      # d will be 1.0
+e = float(2.5)    # e will be 2.5
+f = float("3")    # f will be 3.0
+g = float("4.23") # g will be 4.23
+h = str("80s")    # h will be '80s'
+i = str(22)       # i will be '22'
+j = str(3.01)     # j will be '3.01'
+
+print([a,b,c,c1,d,e,f,g,h,i,j])
