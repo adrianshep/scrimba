7f4790b48e4ae3e5d2529c0475b9c1ad978cafcb
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-03-28 21:38:37 -0400

    split and join - exercise

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -362,3 +362,15 @@ print('-.join(friends_list')
 print(''join(msg.split()))
 # do the same thing with this:
 print(msg.replace(' ', ''))
+
+##############################
+
+# Split and Join - Exercise
+
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+friends_list = ['Exercise: fill me with names']
+print(friends_list)
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
