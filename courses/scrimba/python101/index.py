07c4b0c33aa368847955285a0ea5de5c4ffcdfae
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-04-01 13:36:07 -0400

    sets - exercise - does item exist

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -464,3 +464,24 @@ empty_tuple = tuple()
 #empty Set
 empty_set = {} # this is wrong, this is a dictionary
 empty_set = set()
+
+##############################
+
+# Sets - Exercise
+
+friends = {'John','Michael','Terry','Eric','Graham'}
+my_friends = {'Reg','Loretta','Colin','John','Graham'}
+cars =['900','420','V70','911','996','V90','911','911','S','328','900']
+
+#1. Check if ‘Eric’ and ‘John’ exist in friends
+# my solution
+print('Eric' in friends)
+print('John' in friends)
+# Olof's solution
+print('Eric' in friends and 'John' in friends)
+
+#2. combine or add the two sets 
+#3. Find names that are in both sets
+#4. find names that are only in friends
+#5. Show only the names who only appear in one of the lists
+#6. Create a new cars-list without duplicates
