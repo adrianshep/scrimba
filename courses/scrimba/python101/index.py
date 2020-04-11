df5afe78801abd9a6c5b49ff9b9664da57244221
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-04-11 16:03:09 -0400

    functions - exercise - Olof's solution

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -570,6 +570,8 @@ greeting("Judith")
 
 # Functions - Exercise
 
+# my solution:
+
 def greeting(name, age=28, color='red'):
     #Greets user with 'name' from 'input box' and 'age', if available, default age is used
     age = int(age) + 1
@@ -592,3 +594,16 @@ greeting(name, color, age)
 # 5. Capitalize first letter of the 'name', and rest are small caps 
 # 6. Favorite color should be in lowercase 
 
+# Olof's solution:
+
+def greeting(name, age=28, color = 'red'):
+    #Greets user with 'name' from 'input box' and 'age' next year, if available, default age is used
+    # also includes favorite color
+    print('Hello '  +  name.capitalize() + ', you will be ' + str(age+1) +' next birthday!')
+    print(f'Hello {name.capitalize()}, you will be {age+1} next birthday!')
+    print(f'We hear you like the color {color.lower()}!')
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+color = input('Enter favorite color: ')
+greeting(name, int(age), color)
