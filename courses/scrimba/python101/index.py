584bdc0bf55f1f7702bdce25a1efc66160397e1a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-04-21 13:35:13 -0400

    while loops - exercise - guessing game - Olof's solutions

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -998,3 +998,52 @@ while i < guesses:
     else:
       print('You guessed it!', answer, 'is the number!')
       i = guesses
+
+# Olof's solution:
+
+# Three Loop Questions:
+#1. What do I want to repeat?
+#  -> guesses
+#2. What do I want to change each time?
+#  -> guess number and number of guesses
+#3. How long should we repeat?
+#  -> until user loses, runs out of guesses, or wins
+
+num = 12
+guess = 0
+guess_limit=3
+guess_number = 0
+
+while guess_number < guess_limit:
+    guess = int(input(f'Guess # {guess_number+1} a number 1-20: last guess:{guess} '))
+    if guess == num:
+        print(f'You Win! You Guessed it: {guess}')
+        break
+    else:
+        print(f'No, not {guess}!')
+        guess_number += 1
+if guess != num:
+    print(f'Sorry you lose! It was {num}')
+
+# Olof's modification solution:
+
+num = 76
+guess = 0
+guess_limit=5
+guess_number = 0
+guess = int(input(f'Guess a number 1-100: '))
+guess_number +=1
+while guess_number < guess_limit:
+    
+    if guess != num:
+        guess_number +=1
+        if guess > num:
+            guess = int(input(f'{guess} Too high - Guess again 1-100: '))
+        else:
+            guess = int(input(f'{guess} too low - Guess again 1-100: '))
+    if guess == num:
+        print(f'You Win! You Guessed it: {guess}')
+        break
+    
+if guess != num:
+    print(f'Sorry you lose! It was {num}')
