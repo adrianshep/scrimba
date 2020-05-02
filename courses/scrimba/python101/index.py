0f4440d0f17ff6c5fb6213ceb3e97cce1cf5a1a5
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-05-01 21:42:18 -0400

    dictionaries exercise 1.0 - my solution - concatenating shops

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -1467,7 +1467,32 @@ print('The sum of the ages: ', sum(people.values()))
 # one way to buy by typing the key 'newt' in an input box...or something
 # at end you should print the 'items' you have taken..in this version you don't have to pay for stuff or add it up
 
+# year = movie.pop('year') pop removes and stores item in variable
+# print(movie.get('budget','not found')) # will set the default value as 'not found'
+# print(movie.keys())
+# for key, value in movie.items():
+    # print(key, value)
+
+#create an empty shopping cart
+cart = {}
+#loop through stores/dicts
+for LOOP OVER THE SHOPS :
+    #inputbox  to show what you can buy...capture textstring of what was bought...make lowercase
+    buy_item = input(f'Welcome to {SHOPNAME}! what do you want to buy: {LIST ITEMS FOR SALE})
+    #update the cart
+    cart.update({insert KEYVAL:VALUE}) # use pop...
+print(f'You Purchased {ITEMS PUCHASED} Today it is all free. Have a nice day of mayhem!')
+
 #create stores
 freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
 antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
 pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
+
+# my solution:
+cart = {}
+all_shops = {}
+for shops in (freelancers, antiques, pet_shop):
+  all_shops.update(shops)
+for shop in all_shops:
+  print(shop)
+
