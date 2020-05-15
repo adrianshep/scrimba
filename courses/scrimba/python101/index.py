ee8c8752b08995e234387649ab7940cf123a321d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-05-15 17:15:31 -0400

    classes and objects

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -1665,3 +1665,40 @@ finally:
     #executed if no error
 #finally:
     #always executed 
+
+
+##############################
+
+# Classes and Objects
+
+class Movie:
+    def __init__(self,title,year,imdb_score,have_seen):
+        self.title = title
+        self.year = year
+        self.imdb_score = imdb_score
+        self.have_seen = have_seen
+    
+    def nice_print(self):
+        print("Title: ", self.title)
+        print("Year of production: ", self.year)
+        print("IMDB Score: ", self.imdb_score)
+        print("I have seen it: ", self.have_seen)
+        
+film_1 = Movie("Life of Brian",1979,8.1,True)
+film_2 = Movie("The Holy Grail",1975,8.2,True)
+
+#print(film_1.title, film_1.imdb_score)
+# self is always a reference to object that's active at the moment
+# argument is empty in film_2 call because it's implicit
+film_2.nice_print()
+# above is equivalent to:
+Movie.nice_print(film_2)
+
+films = [film_1,film_2]
+print(films[1].title,films[0].title)
+films[0].nice_print()
+
+#Classes are blueprints
+#Objects are the actual things you built
+#variables => attributes
+#functions => methods
