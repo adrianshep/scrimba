6902d3cfe5d8c68cd63e13e81a005215faad1a56
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-05-20 21:21:54 -0400

    lambda functions part 2

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -1926,3 +1926,45 @@ monty_python.sort(key = lambda name:name.split(' ')[-1])
 print(monty_python)
 monty_python.sort(key= sort_names)
 print(monty_python)
+
+
+##############################
+
+# Lambda Functions Part 2
+
+def func(n):
+    return lambda a: a*n
+
+doubler = func(2)
+print(doubler)
+# returns <function <lambda>>
+
+print(doubler(3))
+# returns 6
+
+quintipler = func(5)
+print(quintipler(3))
+# returns 15
+    
+print(type(func(3)))
+# returns <class 'int'>
+
+def func(n):
+    return lambda a: a*n
+    
+print(type(func(3)))
+# returns <class 'function'>
+
+def price_calc(start,hourly_rate):
+    return lambda hours: start + hourly_rate * hours
+    
+walkin_cost = price_calc(10,30)
+premium_cost = price_calc(1,25)
+print(walkin_cost(2))
+print(premium_cost(2))
+print(price_calc(1,25)(2))
+
+print((lambda a,b,c: a+b+c)(2,3,4))
+print((lambda a,b,c=2: a+b+c)(2,3,4))
+print((lambda a,b,c=2: a+b+c)(2,c=3,b=4))
+print((lambda *args: sum(args))(2,3,4,50))
