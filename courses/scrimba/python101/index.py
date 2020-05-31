29f349d980574bd18f32edcac00d4d82bb1c1993
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-05-31 16:08:32 -0400

    timeit and performance

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -2237,3 +2237,65 @@ word = random.choices(letter_numbers, k=7)
 # strings can be duplicated
 
 
+##############################
+
+# Timeit and Performance
+
+print('Performance and Timeit module')
+# Experiment with sieve of Eratosthenes
+
+# first way
+print([x for x in range(1, 151) if not any([x % y == 0 for y in range(2, x)]) and not x == 1])
+
+# second way starts at 2 and removes the not x == 1
+print([x for x in range(2, 151) if not any([x % y == 0 for y in range(2, x)])])
+
+# third way
+# Initialize a list
+primes = []
+for possiblePrime in range(2, 151):
+# Assume number is prime until shown it is not.
+   isPrime = True
+   for num in range(2, int(possiblePrime ** 0.5) + 1):
+       if possiblePrime % num == 0:
+           isPrime = False
+           break
+   if isPrime:
+       primes.append(possiblePrime)
+print(primes)
+
+# timing each of the three ways
+
+import timeit
+
+print('Performance and Timeit module')
+# Experiment with sieve of Eratosthenes
+def test1():
+    [x for x in range(1, 151) if not any([x % y == 0 for y in range(2, x)]) and not x == 1]
+    return(1)
+def test2():
+    [x for x in range(2, 151) if not any([x % y == 0 for y in range(2, x)])]
+def test3():
+    # Initialize a list
+    primes = []
+    for possiblePrime in range(2, 151):
+    # Assume number is prime until shown it is not.
+        isPrime = True
+        for num in range(2, int(possiblePrime ** 0.5) + 1):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(possiblePrime)
+    #print(primes)
+    return(1)
+
+print(timeit.timeit('test1()', globals=globals(), number=10))
+print(timeit.timeit('test2()', globals=globals(), number=10))
+print(timeit.timeit('test3()', globals=globals(), number=10))
+
+# returns 
+# >Performance and Timeit module
+# >3.819000005722046
+# >3.441999912261963
+# >0.17100000381469727
