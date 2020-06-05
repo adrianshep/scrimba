01b07c6afb2387f582a56b7eceb80ed7c4b8b807
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-06-05 17:38:07 -0400

    project - math tutor - loop through questions

--- courses/scrimba/python101/index.py
+++ courses/scrimba/python101/index.py
@@ -2355,6 +2355,14 @@ no_questions = int(input('How many questions would you like to try?: '))
 score = 0
 
 # loop through number of questions
+for q in range(no_questions):
+    num1,num2 = r(1, 11), r(1, 11)
+    ans = num1 * num2
+    u_ans = int(input(f'{num1} X {num2} = '))
+    if u_ans == ans:
+        score += 1
+print(f'Thank you for playing! \nYou got {score} out of {no_questions} ({round(score/no_questions * 100)}%)')
+
 # create two random numbers and calc answer
 # show user the question
 # capture answer and modify user score
