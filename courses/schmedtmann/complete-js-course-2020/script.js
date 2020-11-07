bdc84ad64527272fb70ddac226315a50a0e4baef
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-11-07 17:32:30 -0500

    js fundamentals part 1 - assignment - values and variables

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -1,2 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+
+let country = 'United States of America';
+let continent = 'North America';
+let population = 330;
+
+console.log(country);
+console.log(continent);
+console.log(population);
