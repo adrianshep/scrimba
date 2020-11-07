fed642b6c690c52bbdca8c6d0a1220db0e6158e0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-11-07 17:39:08 -0500

    js fundamentals part 1 - assignment - data types

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -1,6 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// ASSIGNMENTS
+
+// Values and Variables
+
 let country = 'United States of America';
 let continent = 'North America';
 let population = 330;
@@ -8,3 +12,13 @@ let population = 330;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Data Types
+
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
