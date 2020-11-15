3782bd977668e215ae31de5b89c40ba60e7e852b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-11-15 18:41:02 -0500

    js fundamentals part 1 - assignment - switch statement

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -306,3 +306,29 @@ if (day === 'monday') {
 } else {
     console.log('Not a valid day!');
 }
+
+// Assignment
+// Switch Statement
+
+const language = 'spanish';
+
+switch(language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
