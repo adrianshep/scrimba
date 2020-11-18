8f5760b47890f429d9ec13d665a725038acd8cc9
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-11-18 18:05:48 -0500

    js fundamentals part 2 - functions - parameters and return

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -409,3 +409,11 @@ function logger() {
 logger();
 logger();
 logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+fruitProcessor(5, 0);
