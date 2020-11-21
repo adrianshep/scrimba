98efabaa425f3e19162fd8f75ef72e49df916c17
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-11-21 17:28:45 -0500

    js fundamentals part 2 - functions - further defining functions

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -433,9 +433,12 @@ const age1 = calcAge1(1991);
 console.log(age1);
 
 // function expression
+// expression is unnamed and lies to the right of the assignment operator
+// in JS, functions are also values, and can therefore be stored in the variable
 const calcAge2 = function(birthYear) {
     return 2037 - birthYear;
 }
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
