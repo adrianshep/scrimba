a6b0e8ab5ed6b7ddd24b82f7f7df422dedafa41d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-12-11 17:56:49 -0500

    js fundamentals part 2 - object methods - challenge - my solution

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -746,3 +746,29 @@ console.log(jonas.calcAge());
 console.log(jonas.age);
 console.log(jonas.age);
 console.log(jonas.age);
+
+// Challenge:
+// Write this string:
+// Jonas is a 46 year old teacher and he has a driver's license
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function(birthYear) {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    }
+
+    if (hasDriversLicense) {
+        const driversLicencse = 'has a driver\'s license';
+    } else {
+        const driversLicense = 'doesn\'t have a driver\'s license';
+    }
+};
+
+console.log(`Jonas is a ${age}-year-old ${job}, and he `);
