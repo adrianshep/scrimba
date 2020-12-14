b71ee969ffdc96cc75fd9e92cfe58249825b1c9e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-12-14 18:00:31 -0500

    js fundamentals part 2 - looping arrays, breaking and continuing - another example

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -818,3 +818,12 @@ console.log(types);
 // this approach is cleaner
 
 types.push(typeof jonas[i]);
+
+// another example
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
