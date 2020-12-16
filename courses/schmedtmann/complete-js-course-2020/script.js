21e17bb8042888dd4cc0d072a43e7a099f68e4f8
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-12-16 17:53:56 -0500

    js fundamentals part 2 - looping backwards & loops in loops - loop within loop

--- courses/schmedtmann/complete-js-course-2020/script.js
+++ courses/schmedtmann/complete-js-course-2020/script.js
@@ -861,3 +861,13 @@ const jonas = [
 for (let i = jonas.length - 1; i >= 0; i--) {
     console.log(i, jonas[i]);
 }
+
+// Loop within a loop
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--------- Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Lifting weight repetition ${rep}`);
+    }
+}
