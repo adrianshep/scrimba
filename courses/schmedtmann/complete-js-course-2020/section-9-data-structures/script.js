977e926c060932c3fc46e214fcdf9785cd484e1a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-12-19 22:03:26 -0500

    data structures, modern operators and strings - destructuring arrays - example

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -7,3 +7,15 @@ const restaurant = {
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto']
 };
+
+// Lesson: Destructuring Arrays
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// destructuring array from right side of assignment operator
+// unpacking array, not destroying it
+const [x, y, z] = arr;
+console.log(x, y, z);
