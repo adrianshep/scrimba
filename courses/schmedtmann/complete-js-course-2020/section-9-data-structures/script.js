09da0339a4257765ee8410247d0739785f49f7f1
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-12-25 18:09:57 -0500

    data structures, modern operators and strings - destructuring objects - destructuring via function arguments

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -24,8 +24,19 @@ const restaurant = {
 
     order: function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function({starterIndex, mainIndex, time, address}) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
     }
 };
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+})
 
 // Lesson: Destructuring Arrays
 
@@ -94,3 +105,4 @@ const obj = {a: 23, b: 7, c: 14};
 // nested objects
 const { fri: { open: o, close: c }} = openingHours;
 console.log(open, close);
+
