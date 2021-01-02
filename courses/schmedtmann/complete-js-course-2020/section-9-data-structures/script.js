309b675e6eab80ff514f1884db445179013f8b96
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-01-02 18:01:05 -0500

    data structures, modern operators and strings - rest patterns and parameters - rest in arrays

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -186,3 +186,7 @@ const [a, b, ...others] = [1, 2, 3, 4, 5];
 // result:
 // 1 2 [3, 4, 5]
 console.log(a, b, others);
+
+const [pizza, , risott, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+console.log(pizza, risotto, otherFood);
