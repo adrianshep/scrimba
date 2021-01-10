f18d4c0c63ac0bf71d8e6829974d66fbd7490467
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-01-10 18:20:19 -0500

    data structures, modern operators and strings - ES6 enhanced object literals - opening hours as separate object

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -1,26 +1,27 @@
 'use strict';
 
+const openingHours = {
+    thu: {
+        open: 12,
+        close: 22,
+    },
+    fri: {
+        open: 11,
+        close: 23,
+    },
+    sat: {
+        // open 24 hours
+        open: 0, 
+        close: 24,
+    }
+};
+
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            // open 24 hours
-            open: 0, 
-            close: 24,
-        }
-    },
 
     order: function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -289,3 +290,6 @@ for (const [i, el] of menu.entries()) {
 }
 
 // console.log([...menu.entries()]);
+
+
+// Lesson: Enhanced Object Literals
