923337fd0c392ecf9c7f585051ff99f786fd8e3e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-01-11 18:05:06 -0500

    data structures, modern operators and strings - ES6 enhanced object literals - simplifying methods

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -26,19 +26,19 @@ const restaurant = {
     // ES6 enhanced object literals
     openingHours,
 
-    order: function(starterIndex, mainIndex) {
+    order(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 
-    orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
         console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
     },
 
-    orderPasta: function(ing1, ing2, ing3) {
+    orderPasta(ing1, ing2, ing3) {
         console.log(`Here is our delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
     },
 
-    orderPizza: function(mainIngredient, ...otherIngredients) {
+    orderPizza(mainIngredient, ...otherIngredients) {
         console.log(mainIngredient);
         console.log(otherIngredients);
     }
