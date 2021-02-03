1897ee55c2ffe3b681afb314ef56da92c81a7a4e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-02-03 16:39:48 -0500

    data structures, modern operators and strings - ES6 maps: iteration - convert map to array

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -498,6 +498,13 @@ for (const [key, value] of question) {
     if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
 }
 const answer = Number(prompt('Your answer'));
+const answer = 3;
 console.log(answer);
 
 console.log(question.get(question.get('correct') === answer));
+
+// convert map to array
+console.log([...question]);
+// console.log(question.entries);
+console.log([...question.keys()]);
+console.log([...question.values()]);
