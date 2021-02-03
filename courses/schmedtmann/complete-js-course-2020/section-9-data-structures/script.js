3d0612bbc8ca80c55a4cce3d42a5ca12386d774a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-02-03 16:36:36 -0500

    data structures, modern operators and strings - ES6 maps: iteration - for loop

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -491,3 +491,13 @@ console.log(question);
 console.log(Object.entries(openingHours));
 const hoursMap = new Map(Object.entries(openingHours));
 console.log(hoursMap);
+
+// for loop in Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+    if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
