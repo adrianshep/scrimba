54618e8be913ac1eb796f7402e90daf428370166
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-02-22 17:07:59 -0500

    data structures, modern operators and strings - working with strings - pt 2 - regex alternative to .replaceAll

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -657,3 +657,8 @@ const announcement = 'All passengers come to boarding door 23. Boarding door 23!
 
 console.log(announcement.replace('door', 'gate'));
 console.log(announcement.replaceAll('door', 'gate'));
+
+//  another solution where replaceAll isn't available
+// regex
+// "g" stands for global
+console.log(announcement.replace(/door/g, 'gate'));
