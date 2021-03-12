056075741340d0da5f037cd70b65e45ef1b46709
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-03-12 17:46:29 -0500

    data structures, modern operators and strings - working with strings - coding challenge 4 - .split array

--- courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-9-data-structures/script.js
@@ -777,5 +777,5 @@ document.body.append(document.createElement('button'));
 const text = document.querySelector('textarea').value;
 
 document.querySelector('button').addEventListener('click', function() {
-
+    const text = document.querySelector('textarea').value;const rows = text.split('\n');
 });
