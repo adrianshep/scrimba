c5116d2bfc5701f4c6fbd6c6205410277f76e71a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-04-04 08:52:15 -0400

    a closer look at functions - default parameters - createBooking function

--- courses/schmedtmann/complete-js-course-2020/section-10-functions-closer-look/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-10-functions-closer-look/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const booking = [];
+
+const createBooking = function(flightNumber, numPassengers, price) {
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    booking.push(booking);
+}
