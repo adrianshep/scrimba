4ae8bb539ca4ce110d7b630a3bcdd5ddd2fad35a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-05-10 17:43:04 -0400

    a closer look at functions - bind method - tax function returning VAT function

--- courses/schmedtmann/complete-js-course-2020/section-10-functions-closer-look/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-10-functions-closer-look/script.js
@@ -324,3 +324,14 @@ const addVAT = addTax.bind(null, 0.23);
 
 console.log(addVAT(100));
 console.log(addVAT(23));
+
+// Challenge: rewrite above as one function returning another
+
+const addTaxRate = function(rate) {
+    return function (value) {
+        return value + vallue * rate;
+    }
+}
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
