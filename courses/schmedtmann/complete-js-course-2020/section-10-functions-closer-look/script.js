243cdb1b05b3277fd5cc59349990d9779eddf6e7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-05-13 20:15:21 -0400

    a closer look at functions - coding challenge 1 - get answer

--- courses/schmedtmann/complete-js-course-2020/section-10-functions-closer-look/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-10-functions-closer-look/script.js
@@ -354,10 +354,14 @@ Here are your tasks:
 2. Call this method whenever the user clicks the "Answer poll" button.
 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
 HINT: Use many of the tools you learned about in this and the last section 😉
+
 BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
 BONUS TEST DATA 1: [5, 2, 3]
 BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
 GOOD LUCK 😀
 */
 
@@ -366,4 +370,15 @@ const poll = {
     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
     // This generates [0, 0, 0, 0]. More in the next section.
     answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        // Get answer
+        const answer = Number(
+            prompt(
+                `${this.question}\n${this.options.join('\n')}
+                \n(Write option number)`
+            )
+        );
+
+        // Register answer
+    }
 };
