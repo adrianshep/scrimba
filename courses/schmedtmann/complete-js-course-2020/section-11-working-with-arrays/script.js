822dbdba403cf3be0bde903a227d42c69e055e95
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-06-17 17:36:17 -0400

    working with arrays - simple array methods - looping arrays: foreach - for of loop comparison

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -149,3 +149,12 @@ console.log(letters.join(' - '));
 // LOOPING ARRAYS: FOREACH
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for of loop, for comparison:
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
