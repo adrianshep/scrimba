e527e86fe2f5ad309f20b505ec76cf07c260b7e3
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-06-19 17:31:56 -0400

    working with arrays - simple array methods - looping arrays: for of adding movement number

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -151,11 +151,15 @@ console.log(letters.join(' - '));
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for of loop, for comparison:
-for (const movement of movements) {
+// for (const movement of movements) {
+// adding movement number for clarity:
+for (const [i, movement] of movements.entries())
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    // console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    // console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 }
 
