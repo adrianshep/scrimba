353bf4940420935765aeb89cc43c0cbca38f2292
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-06-24 17:52:18 -0400

    working with arrays - creating DOM elements - dynamic values

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -216,14 +216,13 @@ currenciesUnique.forEach(function(value, _, map) {
 
 const displayMovements = function(movements) {
   movements.forEach(funciton(mov, i) {
+    
 
     const html = `
-    <div class="movements__row">
-    <div class="movements__type movements__type--deposit">2 deposit</div>
-    <div class="movements__date">3 days ago</div>
-    <div class="movements__value">4 000€</div>
+      <div class="movements__row">
+        <div class="movements__type movements__type--deposit">${i + 1}</div>
+        <div class="movements__value">${mov}</div>
     `;
-
   });
 };
 displayMovements(account1.movements);
