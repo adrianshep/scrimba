89b7712707a235b7b3ca66c4a3c00e967f4f17e1
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-06-29 10:23:50 -0400

    working with arrays - coding challenge 1 - slice()

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -251,3 +251,8 @@ TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
 TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
+
+const checkDogs = function(dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+}
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
