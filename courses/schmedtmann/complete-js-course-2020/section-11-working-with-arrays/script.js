cb47561c9c1d96ba40319864ecece1fbfe2d8b9d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-07-01 17:42:37 -0400

    working with arrays - coding challenge 1 - another solution slice() only

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -257,5 +257,8 @@ const checkDogs = function(dogsJulia, dogsKate) {
   dogsJuliaCorrected.splice(0, 1);
   dogsJuliaCorrected.splice(-2);
   // return [5, 2]
+  // another solution using slice only:
+  // dogsJulia.slice(1, 3);
+
 }
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
