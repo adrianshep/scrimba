26afff7cf29de10448f9e048ef8dbd82dc9369bc
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-07-04 16:50:08 -0400

    working with arrays - data transformations: map, filter, reduce - map

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -276,3 +276,11 @@ const checkDogs = function(dogsJulia, dogsKate) {
 // Test Data 2:
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
+
+// DATA TRANSFORMATIONS: MAP, FILTER, REDUCE
+// MAP
+// is similar to the forEach method except that map creates a brand new array based on the original array
+// the map method takes an array, loops over that array and in each iteration, it applies a callback function that we specify in our code to the current array element.
+// original array: [3, 1, 4, 3, 2]
+// MAP: current * 2
+// returns  a brand new array [6, 2, 8, 6, 4]
