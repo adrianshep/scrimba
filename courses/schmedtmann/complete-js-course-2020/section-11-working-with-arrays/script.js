285b000920d68123865ee3f68bfd92f1cf74e432
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-07-31 08:18:33 -0400

    working with arrays - reduce method - calling calcDisplaybalance

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -402,6 +402,7 @@ const calcDisplayBalance = function(movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance} EUR`;
 };
+calcDisplayBalance(account1.movements);
 
 // FILTER METHOD
 
