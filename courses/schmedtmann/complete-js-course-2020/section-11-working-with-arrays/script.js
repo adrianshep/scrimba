3d156ea979bb81bb684c5fc2395c099b3acd17aa
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-08-07 08:42:15 -0400

    working with arrays - coding challenge 2 - calculate ave age - Jonas's solution corrected

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -510,5 +510,8 @@ const calcAverageHumanAge = function(ages) {
 
   // Jonas's solution:
   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  return average;
 };
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
