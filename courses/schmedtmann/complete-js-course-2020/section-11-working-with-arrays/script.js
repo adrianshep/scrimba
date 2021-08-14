6c24f5e0bfe133aeff1d832f500119789c54f92b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-08-14 08:45:50 -0400

    working with arrays - magic of chaining methods - create const out

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -408,7 +408,11 @@ const calcDisplaySummary = function(movements) {
   const incomes = movements
     .filter(mov => 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements.filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0)
+    labelSumOut.textContent = `$(out)€`;
 };
 calcDisplaySummary(account1.movements);
 
