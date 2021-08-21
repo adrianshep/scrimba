0aaadd27b2a1adfc3c0e7432c2f941c833a2953e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-08-21 08:44:36 -0400

    working with arrays - coding challenge 3 - Jonas's solution - averages

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -586,20 +586,9 @@ GOOD LUCK 😀
 // Jonas's solution:
 const calcAverageHumanAge = age => ages
   .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-  .filter(age => age >= 18);
+  .filter(age => age >= 18)
+  .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
-// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-
-// const adults = humanAges.filter(age => age >= 18);
-
-// const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
-
-  // another way to calculate average:
-  // (2 + 3)/2 = 2.5 === 2/2 + 3/2 = 2.5
-  // const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length, 0) / adults.length;
-
-  // return average;
-// };
-// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-// console.log(avg1, avg2);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
