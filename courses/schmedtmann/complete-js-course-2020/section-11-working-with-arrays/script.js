be064805547bd287e0853b51ad1dad2415e93dc0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-09-04 13:34:34 -0400

    working with arrays - implementing transfers - receiverAcc of transfer

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -462,6 +462,8 @@ if (currentAccount?.pin === Number(inputLoginPin.value)) {
 btnTransfer.addEventListener('click', function(e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value);
 });
 
 // FILTER METHOD
