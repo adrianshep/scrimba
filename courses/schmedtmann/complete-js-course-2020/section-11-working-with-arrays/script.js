e7ae7de5db2c08a68c6f1644f8660b2daac5f59c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-11-14 07:26:18 -0500

    working with arrays - array methods practice - convertTitleCase split

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -1050,7 +1050,9 @@ const { deposits, withdrawals } = accounts
   const convertTitleCase = function(title) {
     const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
 
-    const titleCase = title.toLowerCase()
+    const titleCase = title
+    .toLowerCase()
+    .split(' ')
   };
   console.log(convertTitleCase('this is a nice title'));
   console.log(convertTitleCase('this is LONG title but not too long'));
