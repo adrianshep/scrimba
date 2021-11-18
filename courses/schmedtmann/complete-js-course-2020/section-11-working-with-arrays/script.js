4ab7976554334b1a9e2a4d2fcbf870e29b730aad
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-11-18 08:15:07 -0500

    working with arrays - array methods practice - convertTitleCase first letter of first word capitalized

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -1048,16 +1048,18 @@ const { deposits, withdrawals } = accounts
   // String Conversion to Title Case
   // this is a nice title -> This Is a Nice Title
   const convertTitleCase = function(title) {
+    const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
     const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
 
     const titleCase = title
       .toLowerCase()
       .split(' ')
       .map(word => 
-        exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
-      )
+        (exceptions.includes(word) ? word : capitalize(word)))
       .join(' ');
-    return titleCase;
+
+    return capitalize(titleCase);
   };
   console.log(convertTitleCase('this is a nice title'));
   console.log(convertTitleCase('this is LONG title but not too long'));
