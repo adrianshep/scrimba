c6ddb705cdb535b9c6dc4d6ca4a09a34419fbf11
Adrian Shepelavey <adrianshepelavey@gmail.com>
2021-11-20 21:54:32 -0500

    working with arrays - array methods practice - coding challenge #4 - forEach

--- courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-11-working-with-arrays/script.js
@@ -1067,7 +1067,7 @@ const { deposits, withdrawals } = accounts
 
   console.log(convertTitleCase('and here is another title with an EXAMPLE'));
 
-  
+
 ///////////////////////////////////////
 // Coding Challenge #4
 
@@ -1094,3 +1094,13 @@ const dogs = [
 ];
 GOOD LUCK 😀
 */
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+dogs.forEach(dog => dog.recFood = dog.weight ** 0.75 * 28);
+console.log(dogs);
