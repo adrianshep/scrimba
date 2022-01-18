7e8a587c1ec8225afd9cc23cd1a607258f5c9321
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-01-18 08:13:02 -0500

    numbers, dates, intl, timers - adding dates to app - add displayDate to html

--- courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
@@ -89,11 +89,14 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
+    const date = new Date(acc.movementsDates[i]);
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+        <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
