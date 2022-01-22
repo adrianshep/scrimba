8fdbf59731dc8936db0f549da498143f4d42a644
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-01-22 08:46:27 -0500

    numbers, dates, intl, timers - adding dates to app - correct to .toISOString()

--- courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
@@ -226,8 +226,8 @@ btnTransfer.addEventListener('click', function (e) {
     receiverAcc.movements.push(amount);
 
     // Add transfer date
-    currentAccount.movementsDates.push(new Date().toISOString);
-    receiverAcc.movementsDates.push(new Date().toISOString);
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -244,7 +244,7 @@ btnLoan.addEventListener('click', function (e) {
     currentAccount.movements.push(amount);
 
     // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString);
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
