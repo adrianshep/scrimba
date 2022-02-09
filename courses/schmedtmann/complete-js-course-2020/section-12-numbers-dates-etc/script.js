613b7f0fa852e15aec54b9f9fe933845812ac0ed
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-02-09 08:28:47 -0500

    numbers, dates, intl, timers - internationalizing numbers (Intl) - get locale and language from browser

--- courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
@@ -759,7 +759,13 @@ const num = 3884764.23;
 
 console.log('US: ', new Intl.NumberFormat('en-US').format(num));
 // returns US format: 3,884,764.23
-console.log('US: ', new Intl.NumberFormat('de-DE').format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE').format(num));
 // returns European format: 3.884.764,23
-console.log('US: ', new Intl.NumberFormat('ar-SY').format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY').format(num));
 // returns Arabic format with Arabic numbers and letters
+
+// getting locale from browser:
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language).format(num)
+  );
