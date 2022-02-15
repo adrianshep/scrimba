74bbf9909c1f35a7c7179865b9ef76f8a9827c86
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-02-15 08:28:51 -0500

    numbers, dates, intl, timers - internationalizing numbers (Intl) - formatCur in formattedMov

--- courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
@@ -120,11 +120,8 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(acc.movementsDates[i]);
     const displayDate = formatMovementDate(date, acc.locale);
 
-    const formattedMov = new Intl.NumberFormat(acc.locale, {
-      style: 'currency',
-      currency: acc.currency
-    }).format(mov);
-
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+    
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -141,6 +138,8 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  const formattedMov = formatCur(acc.bal, acc.locale, acc.currency);
+
   labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
