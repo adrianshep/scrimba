c7f562ef54c697c5a45b3ec43852bf7e93810fce
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-02-22 08:08:48 -0500

    numbers, dates, intl, timers - timers: setTimeout and setInterval - wrap bank pushes and update in setTimeout

--- courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
@@ -300,14 +300,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function() {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -828,4 +830,4 @@ console.log(
   );
   console.log('Waiting...');
 
-  if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+  if (ingredients.includes('spinach')) clearTimeout(pizzaTimer)
