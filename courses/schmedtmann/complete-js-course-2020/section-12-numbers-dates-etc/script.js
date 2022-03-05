660af71308ee1c0c8a3ba50ea96f0a351386554f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-03-05 09:05:11 -0500

    numbers, dates, intl, timers - implementing a countdown timer - activity timer reset

--- courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-12-numbers-dates-etc/script.js
@@ -328,6 +328,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
