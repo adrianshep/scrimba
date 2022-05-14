c233b3eb469b3fb243666455d0b981ec6c0f8fcd
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-05-14 08:17:46 -0400

    advanced dom and events - passing arguments into event handlers - placing all selections at top

--- courses/schmedtmann/complete-js-course-2020/section-13-adv-dom-bankist/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-13-adv-dom-bankist/script.js
@@ -7,6 +7,9 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window
