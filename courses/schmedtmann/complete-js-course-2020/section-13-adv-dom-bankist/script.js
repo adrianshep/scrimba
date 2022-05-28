a9df0d742b9c0b973c45f9c56f65c23444dee5b8
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-05-28 12:04:29 -0400

    advanced dom and events - better way: intersection observer api - create obsCallback and obsOptions

--- courses/schmedtmann/complete-js-course-2020/section-13-adv-dom-bankist/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-13-adv-dom-bankist/script.js
@@ -306,7 +306,11 @@ window.addEventListener('scroll', function(e) {
 
 // this API allows our code to observe changes to the way a certain target element intersects another or the viewport
 
-const observer = new IntersectionObserver();
+const obsCallback = function() {};
+
+const obsOptions = {};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(section1);
 
 
