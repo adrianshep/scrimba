5f5f160d76d397f72d053e35afe1a9feaeb4cc1d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-07-08 08:03:06 -0400

    advanced dom and events - building slider pt 2 - create dotContainer.addEventListener()

--- courses/schmedtmann/complete-js-course-2020/section-13-adv-dom-bankist/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-13-adv-dom-bankist/script.js
@@ -483,6 +483,12 @@ document.addEventListener('keydown', function(e) {
 
 // if curSlide = 1, then: -100%, 0%, 100%, 200%, 300%
 
+dotContainer.addEventListener('click', function(e) {
+  if (e.target.classList.contains('dots__dot')) {
+    console.log('DOT');
+  }
+});
+
 // Selecting, Creating, and Deleting Elements
 
 // Selecting elements
