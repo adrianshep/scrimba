0d57995f8a872052f59e5b01533c59fe1229142c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-08-07 09:35:17 -0400

    object-oriented programming - what is oop?

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+// What is Object-Oriented Programming (OOP)?
+
+// OOP is a programming paradigm based on the concept of objects;
+// We use objects to model (describe) real-word or abstract features;
+// Objects may contain data (properties) and code (methods). By using objects, we pack data and the corresponding behavior into one block;
+// In OOP, objects are self-contained pieces/blocks of code;
+// Objects are building blocks of applications, and interact with one another;
+// Interactions happen through a public interface (API); methods that the code outside of the object can access and use to communicate with the object;
+// OOP was developed with the goal of organizing code to make it more flexible and easier to maintain (avoid "spaghetti code").
