a8736a9a650a445a11ae055e05e9419f30190f68
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-08-08 08:11:15 -0400

    object-oriented programming - what is oop? - classes

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -10,3 +10,24 @@
 // Objects are building blocks of applications, and interact with one another;
 // Interactions happen through a public interface (API); methods that the code outside of the object can access and use to communicate with the object;
 // OOP was developed with the goal of organizing code to make it more flexible and easier to maintain (avoid "spaghetti code").
+
+// Functional Programming is another popular programming paradigm that we will focus on later
+
+// Classes and Instances (Traditional OOP)
+// Classes
+// like a blueprint from which we can create new objects
+// (below is a representation only -- JS does NOT support real classes as represented here)
+// User {
+//     user
+//     password
+//     email
+// }
+// 
+// login(password) {
+    // Login logic
+// }
+// 
+// sendMessage(str) {
+    // Sending logic
+// }
+
