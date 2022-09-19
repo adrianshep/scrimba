824f54a7f1083a1468cb05396ac44818809592f9
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-09-19 08:52:06 -0400

    object-oriented programming - coding challenge 1

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -516,3 +516,18 @@ console.dir(x => x + 1);
 // __proto__: will contain methods we have used previously on functions
 // this is the reason why we can call methods on functions -- they are objects and objects have prototypes
 // (and, in this case, a function prototype) 
+
+
+// Coding Challenge #1
+/*
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
