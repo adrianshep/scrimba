39f202cf3dbec192cd5ebffbbaf8c79631e74a98
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-09-23 07:58:43 -0400

    object-oriented programming - coding challenge 1 - implement accelerate method solution

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -542,6 +542,12 @@ Car.prototype.accelerate = function() {
     console.log(10 + this.speedKmh);
 };
 
+// solution:
+// Car.prototype.accelerate = function() {
+//      this.speed += 10;
+//      console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+
 /*
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
