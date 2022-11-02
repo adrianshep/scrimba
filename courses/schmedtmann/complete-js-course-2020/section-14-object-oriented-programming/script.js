8c39857a63e58f2b44466cc748a7b06ff94addf4
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-11-02 08:00:08 -0400

    object-oriented programming - coding challenge 2 - add speedUS getter solution

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -940,6 +940,11 @@ get speedUS() {
     return this.speed/1.6;
 }
 
+// solution:
+// get speedUS() {
+//     return this.speed / 1.6;
+// }
+
 /*
 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
 4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
