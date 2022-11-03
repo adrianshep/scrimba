48296dafabca4ac4719295fe00b10748b725d10d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-11-03 07:42:17 -0400

    object-oriented programming - coding challenge 2 - add speedUS setter

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -947,6 +947,13 @@ get speedUS() {
 
 /*
 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+*/
+
+set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+
+/*
 4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
 DATA CAR 1: 'Ford' going at 120 km/h
 GOOD LUCK 😀
