344b2aeca9b96e076baadbd20bc783221eac8056
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-11-12 15:12:14 -0500

    object-oriented programming - inheritance between classes: constructor functions - add introduce method

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1009,6 +1009,10 @@ const Student = function(firstName, birthYear, course) {
     this.firstName = firstName;
     this.birthYear = birthYear;
     this.course = course;
+};
+
+Student.prototype.introduce = function() {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
 }
 
 const mike = new Student('Mike', 2020, 'Computer Science');
