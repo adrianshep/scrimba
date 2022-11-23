f6edba077d58d74f5210d16f25ed832256a2e433
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-11-23 08:27:34 -0500

    object-oriented programming - coding challenge 3 - constructor EVCl

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1080,6 +1080,17 @@ console.log(mike instanceof Object);
 
 /* 
 1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+*/
+
+const EVCl = function(make, speed, charge) {
+    this.make = make;
+    this.speed = speed;
+    this.charge = charge;
+};
+
+EVCl.prototype = Object.create(CarCl.prototype);
+
+/*
 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
