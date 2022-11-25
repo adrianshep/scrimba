be554de5ca626de005ae1531547e0da0d8a2959f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-11-25 12:34:46 -0500

    object-oriented programming - coding challenge 3 - implement chargeBattery function

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1098,6 +1098,12 @@ EVCl.prototype = Object.create(CarCl.prototype);
 
 /*
 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+*/
+
+EV.prototype.chargeBattery = function(chargeTo) {
+    this.charge = chargeTo;
+
+/*
 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
 DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
