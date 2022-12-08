c9270c602a4de957b328e8ddf892c4dda4fa834f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2022-12-08 11:19:41 -0500

    object-oriented programming - inheritance between classes: es6 classes - add introduce method

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1201,9 +1201,15 @@ class StudentCl extends PersonCl {
         // the call to the super enables the this kw to be accessed (although accessing the this kw isn't mandatory):
         this.course = course;
     }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study ${this.course}`);
+}
 }
 
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 // if we do not need any new properties and everything inside class StudentCl extends PersonCl -- constructor() {super()} etc. -- is commented out, martha entered in the console will still return: 
 // StudentCl {_fullName: "Martha Jones", birthYear: 2012}
 // no need to bother writing a constructor in the child class
+martha.introduce();
+// returns My name is Martha Jones and I study Computer Science
