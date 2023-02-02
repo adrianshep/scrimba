079d035b1d4e48c1f6a9c62af0c571e6e0dd5166
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-02-02 08:25:34 -0500

    coding challenge 4: create child of parent class

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1696,6 +1696,13 @@ const student = new Student('Jonas', 2020, 2037, 'Medicine');
 
 /* 
 1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+
+// my solution:
+class EVCl extends CarCl {
+    
+}
+
+
 2. Make the 'charge' property private;
 3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
 DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
