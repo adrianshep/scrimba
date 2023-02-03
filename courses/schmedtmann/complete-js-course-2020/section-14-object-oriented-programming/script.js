d8cff1538b259d1fed915ac6c1d60e7884b01ba4
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-02-03 08:16:43 -0500

    coding challenge 4: make charge property private

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1696,14 +1696,18 @@ const student = new Student('Jonas', 2020, 2037, 'Medicine');
 
 /* 
 1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
 
 // my solution:
 class EVCl extends CarCl {
-    
+    #charge = 0;
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
 }
 
-
-2. Make the 'charge' property private;
 3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
 DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
