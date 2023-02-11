a72df9a0130ab3935c548e865a201a693446d875
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-02-11 08:10:03 -0500

    coding challenge 4: truing up code

--- courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-14-object-oriented-programming/script.js
@@ -1785,20 +1785,19 @@ class CarCl {
     }
 }
 
-const EV = function (make, speed, charge) {
-    Car.call(this, make, speed);
-    this.charge = charge;
-};
+class EVCl extends CarCl {
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.charge = charge;
+    }
 
-// Link the prototypes
-EV.prototype = Object.create(Car.prototype);
+    chargeBattery(chargeTo) {
+        this.charge = chargeTo;
+    }
 
-EV.prototype.chargeBattery = function (chargeTo) {
-    this.charge = chargeTo;
-};
-
-EV.prototype.accelerate = function() {
-    this.speed +=20;
-    this.charge--;
-    console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}.`);
+    accelerate() {
+        this.speed += 20;
+        this.charge--;
+        console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`);
+    }
 }
