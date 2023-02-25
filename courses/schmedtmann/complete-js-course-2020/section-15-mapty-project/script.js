3db0b8dd3b944aade4ac67217bec0691f6e30a7a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-02-25 08:42:02 -0500

    project planning architecture

--- courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
@@ -23,4 +23,7 @@ That high level overview allows developers to determine the exact features that 
 3. Flowchart
 What we're going to build: to visualize the different actions that a user can take and how the program should react to these actions, we usually put all these features into a flow chart.
 
+4. Architecture
+How we're going to build it: how we will organize our code and what JavaScript features we will use. The project's architecture holds all the code together. It provides a structure through which an application's functionality can be developed.
+
 */
