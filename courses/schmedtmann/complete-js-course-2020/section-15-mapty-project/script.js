ed76613a0cfdc578db89010d0efe88cc6da2d47c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-04-02 16:22:09 -0400

    Leaflet Library map starter code

--- courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
@@ -169,7 +169,20 @@ if (navigator.geolocation)
         // ultimately, want to load and center the map on this position
         // Google Maps for now:
         // console.log(`https://www.google.com/maps/@${latitude},@${longitude}`);
-        }, 
+        },
+        
+        // Leaflet starter code:
+        var map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+        },
+            
         // error:
         function() {
             alert('Could not get your position')
