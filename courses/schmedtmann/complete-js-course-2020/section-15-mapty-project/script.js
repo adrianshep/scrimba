eb29226e03e6070c6111f5557fa5d3be57ed1fe4
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-07-15 08:00:27 -0400

    rendering workouts create html

--- courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
@@ -225,7 +225,24 @@ _renderWorkoutMarker(workout) {
 
 _renderWorkout(workout) {
 
+    const html = `
+        <li class="workout workout--running" data-id="1234567890">
+          <h2 class="workout__title">Running on April 14</h2>
+          <div class="workout__details">
+            <span class="workout__icon">🏃‍♂️</span>
+            <span class="workout__value">5.2</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">24</span>
+            <span class="workout__unit">min</span>
+          </div>
+    `;
+
 }
+
+
 // all above is but a blueprint
 // to make the application work, we need to create an object:
 
