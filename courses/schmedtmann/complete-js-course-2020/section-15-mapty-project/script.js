23d77747ffa8407c09ff97d089e8ba9ce2c1da38
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-07-27 09:09:03 -0400

    rendering workouts fix error toUpperCase

--- courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-15-mapty-project/script.js
@@ -25,7 +25,7 @@ class Workout {
         // prettier-ignore
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-        this.description = `${this.type[0].toUppercase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
     }
 }
 // we will never directly create a workout
