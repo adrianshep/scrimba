5835629149641983b0896dde867c21b769d7963c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-09-09 08:10:26 -0400

    asynchronous define synchronous

--- courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
@@ -3,4 +3,17 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+// Synchronous Code
+const p = document.querySelector('.p');
+p.textContent = 'My name is Jonas!';
+alert('Text set!');
+p.computedStyleMap.color = 'red';
+
+// most code is synchronous
+// executed line by line in exact order we defined in our code
+// each line of code always waits for previous line to finish execution
+// this can create problems:
+// in example, alert statement will block the rest of the code executing until OK button is clicked
+// most of time, synchronous code is fine and makes perfect sense
+
 ///////////////////////////////////////
