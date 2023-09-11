46b2860a1d39d7a46279945cc906d876e735d5b2
Adrian Shepelavey <adrianshepelavey@gmail.com>
2023-09-11 08:32:33 -0400

    asynchronous callback function agnostic

--- courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
@@ -31,6 +31,15 @@ p.style.color = 'red';
 // the callback function is asynchronous JS
 // it will not be executed immediately, but only after the timer has finished running in the background -- asynchronously
 // it's non-blocking -- the rest of the code can keep running normally
+// in summary, asynchronous programming is about coordinating the behavior of our program over a certain period of time
+
+
+// Callback Functions
+
+// in the above example, we need a callback function to implement the asynchronous behavior
+// however, that doesn't mean that callback functions automatically make code asynchronous
+// for example, the Array map method accepts a callback function, but that doesn't make that code asynchronous
+
 
 ///////////////////////////////////////
 
