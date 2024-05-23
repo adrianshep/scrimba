ca0cc64d51d4882ef1c5f020b733b47323c8bc9b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2024-05-23 08:29:49 -0400

    running promises in parallel destructure and log capitals

--- courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
@@ -1514,10 +1514,18 @@ console.log('3: Finished getting location');
 // for data retrieval and conversion, let's use the getJSON function we coded at the beginning of this section
 // the getJSON function encapsulates the fetch request and error handling, and also immediately converst the response to JSON
 // we will store that data into the variable c1
+// the result of this will be an array of one object
+// use destructuring to get the first element
+// duplicate this code twice to get three countries data returned
+// console log capitals of each of the three countries
 
 const get3Countries = async function(c1, c2, c3) {
     try {
-        const data1 = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`); 
+        const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`);
+        const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`);
+        const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`); 
+
+        console.log([data1.capital, data2.capital, data3.capital])
     } catch(err) {
         console.error(err);
     }
