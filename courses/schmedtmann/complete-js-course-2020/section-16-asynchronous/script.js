0140657ad47664e9d3167e1ea77f77bdd23e46cd
Adrian Shepelavey <adrianshepelavey@gmail.com>
2024-05-27 06:38:40 -0400

    running promises in parallel handle data same way

--- courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-16-asynchronous/script.js
@@ -1527,20 +1527,22 @@ console.log('3: Finished getting location');
 // to do that, we use the Promise.all combinator function
 // which is a helper function and static method on the Promise constructor
 // it takes in an array of promises and returns a new promise, which will then run all the promises in the array at the same time
+// we can then handle that promise the same way we did before
+// call the result data, await it and then log the data to the console
 
 const get3Countries = async function(c1, c2, c3) {
     try {
         // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`);
         // const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`);
         // const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`); 
+        // console.log([data1.capital, data2.capital, data3.capital]);
 
-        Promise.all([
+        const data = await Promise.all([
             getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
             getJSON(`https://restcountries.eu/rest/v2/name/${c21}`),
             getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
         ]);
-
-        console.log([data1.capital, data2.capital, data3.capital])
+    console.log(data);
     } catch(err) {
         console.error(err);
     }
