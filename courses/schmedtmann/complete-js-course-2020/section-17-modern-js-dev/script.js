38ae66b88721de8aaeeeda330b20d165d9c6cd99
Adrian Shepelavey <adrianshepelavey@gmail.com>
2024-06-28 09:03:36 -0400

    modern js dev modules paradigm

--- courses/schmedtmann/complete-js-course-2020/section-17-modern-js-dev/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-17-modern-js-dev/script.js
@@ -1,1 +1,11 @@
 
+
+// Modern JavaScript Development
+
+// code used to be written in one or a few big scripts, which would then be sent to a browser
+// currently, we divide our projects into multiple modules that can share data between themselves
+// this makes our code more organized and maintainable
+// a great thing about modules is that we can also inclue third party modules in our code
+// there are thousands of open source modules which we call packages that developers share on the NPM repository
+// we can use these packages for free in our own code
+// for example, the React framework or jQuery, or even the Leaflet library that we used in our Mapty project
