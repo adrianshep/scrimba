30ac6ea7e6feb7dc0ae3d1e936fcec48c5ee8ad0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2024-06-29 08:03:27 -0400

    modern js dev node package manager

--- courses/schmedtmann/complete-js-course-2020/section-17-modern-js-dev/script.js
+++ courses/schmedtmann/complete-js-course-2020/section-17-modern-js-dev/script.js
@@ -9,3 +9,8 @@
 // there are thousands of open source modules which we call packages that developers share on the NPM repository
 // we can use these packages for free in our own code
 // for example, the React framework or jQuery, or even the Leaflet library that we used in our Mapty project
+// NPM stands for Node Package Manager, originally developed together with and for Node.js
+// however, NPM has established itself as the go-to depository for all kinds of packages in modern JS development
+// in order to download, use and share packages, we use NPM software installed on our computer
+// it's a simple command line interface allowing us to do all that
+// NPM is both a repository in which packages live and a program we use on our computers to install and manage these packages
