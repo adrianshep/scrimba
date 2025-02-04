e8dc4676be2411087aba560c6b848a9fb7081106
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-02-04 07:43:05 -0500

    forkify: app functionality

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/controller.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/controller.js
@@ -21,3 +21,6 @@ const timeout = function (s) {
 // this application is all about searching for recipes and displaying them in the user interface
 // enter "pizza" into search field and left sidebar appears with results
 // when there are more than ten results, they get divided up into multiple pages with pagination functionality allowing you to move from page to page
+// clicking on recipe in sidebar, it displays in center right of page
+// cooking time, number of servings, ingredients list, the link to the recipe source page are all displayed
+// amount of servings desired can be increased and decreased which then revised the quantities in the ingredients list
