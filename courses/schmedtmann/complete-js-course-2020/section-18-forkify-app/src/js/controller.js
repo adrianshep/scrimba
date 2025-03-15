d15c0e3ce48a993c1d9c176406f539963ba58045
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-03-15 08:19:27 -0400

    rendering recipe: publisher and source URL

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/controller.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/controller.js
@@ -339,12 +339,12 @@ const timeout = function (s) {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+            href="${recipe.sourceUrl}"
             target="_blank"
           >
             <span>Directions</span>
@@ -388,3 +388,6 @@ const timeout = function (s) {
 // the recipe title we will use in place of the text string and as an alt text of the image
 // then we have the clock icon, so the 45 is the cooking time -- we replace it with ${recipe.cookingTime}
 // further down we have servings; the static 4 will be replaced with data coming from the recipe object, recipe.servings
+// right now we have two list elements for the static ingredients example, one element for each ingredient, but we'll tackle that later
+// we change recipe publisher to recipe.publisher
+// with the source URL, we have the URL of the recipe itself, where it comes from -- we replace that with recipe.sourceUrl
