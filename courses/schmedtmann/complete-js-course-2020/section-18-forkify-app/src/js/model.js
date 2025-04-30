fef3b7fb1fbe2ca199da1e1fdb92ce8c55c803a7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-04-30 08:10:31 -0400

    refactoring MVC: use previous getting data code

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
@@ -1,7 +1,22 @@
-const state = {
+export const state = {
     recipe: {},
 };
 
 export const loadRecipe = async function() {
-    
+    const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
+
+    if(!res.ok) throw new Error(`${data.message} (${res.status})`)
+
+    let {recipe} = data.data;
+    recipe = {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        sourceUrl: recipe.source_url,
+        image: recipe.image_url,
+        servings: recipe.servings,
+        cookingTime: recipe.cooking_time,
+        ingredients: recipe.ingredients,
+    };
 };
