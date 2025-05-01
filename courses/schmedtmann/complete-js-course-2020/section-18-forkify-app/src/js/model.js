c9bdebaaa8d26b6347b97b27da5eda6b66adae24
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-05-01 08:42:16 -0400

    refactoring MVC: adjustments to getting data code

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
@@ -2,14 +2,14 @@ export const state = {
     recipe: {},
 };
 
-export const loadRecipe = async function() {
-    const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+export const loadRecipe = async function(id) {
+    const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
     const data = await res.json();
 
     if(!res.ok) throw new Error(`${data.message} (${res.status})`)
 
-    let {recipe} = data.data;
-    recipe = {
+    const {recipe} = data.data;
+    state.recipe = {
         id: recipe.id,
         title: recipe.title,
         publisher: recipe.publisher,
@@ -19,4 +19,5 @@ export const loadRecipe = async function() {
         cookingTime: recipe.cooking_time,
         ingredients: recipe.ingredients,
     };
+    console.log(state.recipe);
 };
