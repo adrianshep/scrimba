2067f12f3b3a0b3a6301cd74532f064fceb1fdf6
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-06-08 07:35:57 -0400

    refactoring MVC: fix null problem in npm Fraction

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
@@ -86,7 +86,7 @@ class RecipeView {
               <svg class="recipe__icon">
                 <use href="src/img/icons.svg#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${new Fraction(ing.quantity).toString}</div>
+              <div class="recipe__quantity">${ ing.quantity ? new Fraction(ing.quantity).toString() : '' }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">g</span>
                 pasta
