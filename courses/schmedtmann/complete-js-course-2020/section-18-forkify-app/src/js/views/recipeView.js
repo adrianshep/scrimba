b2b31a228f9065ce2fe0aac0d246d3b0d55c56b6
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-06-10 07:58:04 -0400

    refactoring MVC: refactor function to #generateMarkupIngredient()

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
@@ -83,21 +83,8 @@ class RecipeView {
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
             ${this.#data.ingredients
-                .map(ing => {
-                    return `
-                        <li class="recipe__ingredient">
-                            <svg class="recipe__icon">
-                                <use href="src/img/icons.svg#icon-check"></use>
-                            </svg>
-                            <div class="recipe__quantity">${ ing.quantity ? new Fraction(ing.quantity).toString() : '' }</div>
-                            <div class="recipe__description">
-                                <span class="recipe__unit">${ing.unit}</span>
-                                ${ing.description}
-                            </div>
-                        </li>
-                    `;
-                })
-            .join(')}
+                .map()
+                .join('')}
         </div>
             <li class="recipe__ingredient">
               <svg class="recipe__icon">
@@ -131,5 +118,22 @@ class RecipeView {
           </a>
         </div>
      `;
+    }
+
+    #generateMarkupIngredient(ing) {
+        return `
+            <li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                    <use href="src/img/icons.svg#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">${ ing.quantity ? new Fraction(ing.quantity).toString() : '' }</div>
+                <div class="recipe__description">
+                    <span class="recipe__unit">${ing.unit}</span>
+                    ${ing.description}
+                </div>
+            </li>
+        `;
+    }
 }
+
 export default new RecipeView();
