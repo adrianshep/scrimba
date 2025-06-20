87601dfa01e253b2131cdb7c41a02ae387fcb5f5
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-06-20 08:16:02 -0400

    helpers config files: try catch getJSON function in helpers.js

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/helpers.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/helpers.js
@@ -1,6 +1,11 @@
-export const getJSON = async function (url){
-    const res = await fetch(url);
-    const data = await res.json();
+export const getJSON = async function (url) {
+    try {
+
+        const res = await fetch(url);
+        const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    } catch(err) {
+
+    }
 };
