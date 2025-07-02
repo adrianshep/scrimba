9a99b68f698b7a04d036ab03dc14a1bf6f1cc561
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-07-02 08:08:43 -0400

    helpers config files: add timeout to helpers.js

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/helpers.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/helpers.js
@@ -1,3 +1,11 @@
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+      setTimeout(function () {
+        reject(new Error(`Request took too long! Timeout after ${s} second`));
+      }, s * 1000);
+    });
+  };
+
 export const getJSON = async function (url) {
     try {
 
@@ -10,4 +18,3 @@ export const getJSON = async function (url) {
         throw err;
     }
 };
-
