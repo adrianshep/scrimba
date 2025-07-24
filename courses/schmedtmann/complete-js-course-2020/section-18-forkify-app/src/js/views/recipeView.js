b669292f359d0ae68733954e0fd4e7855e2f5ae1
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-07-24 07:26:44 -0400

    error & success messages: default error message

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
@@ -4,6 +4,7 @@ import {Fraction} from 'fractional';
 class RecipeView {
     #parentElement = document.querySelector('.recipe');
     #data;
+    #errorMessage = 'We could not find that recipe. Please try another one!';
 
     render(data) {
         this.#data = data;
@@ -28,7 +29,7 @@ class RecipeView {
         this.#parentElement.insertAdjacentHTML('afterbegin', markup);
     };
 
-renderError(message) {
+renderError(message = this.#errorMessage) {
   const markup = `
     <div class="error">
       <div>
