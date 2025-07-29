651c61b7561e1e885cc13500b0698a4da9a24a67
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-07-29 07:12:37 -0400

    search results 1: loadSearchResults async function

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
@@ -1,4 +1,4 @@
-// import { async } from 'regenerator-runtime';
+import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
 
@@ -24,7 +24,11 @@ export const loadRecipe = async function(id) {
         console.log(state.recipe);
     } catch (err) {
         // Temporary error handling
-        console.error(`${err} 💥💥💥💥`);        
+        console.error(`${err} 💥💥💥💥`);
         throw err;
     }
 };
+
+export const loadSearchResults = async function(query) {
+    
+}
