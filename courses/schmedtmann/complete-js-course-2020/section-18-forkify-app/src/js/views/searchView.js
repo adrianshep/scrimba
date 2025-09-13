110668c98c7f6f549a39a625428f5be1f84d7a8c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-09-13 08:43:38 -0400

    search results 1: return query in searchView.js

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/searchView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/searchView.js
@@ -2,18 +2,20 @@ class SearchView {
     #parentEl = document.querySelector('.search');
 
     getQuery() {
-    const query = this.#parentEl.querySelector('.search__field').value;
-    this.#clearInput();
+        return this.#parentEl.querySelector('.search__field').value;
+        this.#clearInput();
+        return query;
+    }
 
     #clearInput() {
-        this.#parentEl.querySelector('.search__field').value = '';
+        this.#parentEl.querySelector('search__field').value = '';
     }
     
     addHandlerSearch(handler){
         this.#parentEl.addEventListener('submit', function(e) {
         e.preventDefault();
           handler();
-        })
+        });
     }
 }
 
