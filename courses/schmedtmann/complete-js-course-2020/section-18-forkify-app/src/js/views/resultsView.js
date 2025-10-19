b9ade18ce3eab6050d3359f175efb9a24ce05959
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-10-19 07:27:28 -0400

    search results 2: create generateMarkup in resultsView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
@@ -2,6 +2,10 @@ import View from './View.js';
 
 class ResultsView extends View {
     _parentEl = document.querySelector('.results');
+
+    _generateMarkup() {
+        return
+    }
 }
 
 export default new ResultsView();
