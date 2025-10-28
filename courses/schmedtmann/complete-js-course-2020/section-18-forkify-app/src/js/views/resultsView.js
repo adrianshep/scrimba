926b3034800ea475ed2f01ac4131b7edfba0660a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-10-28 08:28:31 -0400

    search results 2: create _generateMarkupPreview() in resultsView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
@@ -5,26 +5,28 @@ class ResultsView extends View {
 
     _generateMarkup() {
         return this._data.map().join('');
-        
+    }
+
+    _generateMarkupPreview() {
         return `
             <li class="preview">
-            <a class="preview__link preview__link--active" href="#23456">
-              <figure class="preview__fig">
-                <img src="src/img/test-1.jpg" alt="Test" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
-                <p class="preview__publisher">The Pioneer Woman</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href="src/img/icons.svg#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
-            </a>
-          </li>
-        `
-    }
+                  <a class="preview__link preview__link--active" href="#23456">
+                    <figure class="preview__fig">
+                      <img src="src/img/test-1.jpg" alt="Test" />
+                    </figure>
+                    <div class="preview__data">
+                      <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
+                      <p class="preview__publisher">The Pioneer Woman</p>
+                      <div class="preview__user-generated">
+                        <svg>
+                          <use href="src/img/icons.svg#icon-user"></use>
+                        </svg>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+          `;
+      }      
 }
 
 export default new ResultsView();
