0d3e69d928c9d837f3563d2ccb2f464b7f7894c4
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-11-10 08:02:01 -0500

    search results 2: place icons in use href

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
@@ -1,5 +1,4 @@
 import View from './View.js';
-import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
     _parentEl = document.querySelector('.results');
@@ -20,7 +19,7 @@ class ResultsView extends View {
                       <p class="preview__publisher">${result.publisher}</p>
                       <div class="preview__user-generated">
                         <svg>
-                          <use href="src/img/icons.svg#icon-user"></use>
+                          <use href="${icons}#icon-user"></use>
                         </svg>
                       </div>
                     </div>
