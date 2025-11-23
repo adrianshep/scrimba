5dbb2c17cbd9a21fc11a2e4aa5a4ca1180483342
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-11-23 05:54:38 -0500

    search results 2: create error message in resultsView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/resultsView.js
@@ -2,6 +2,8 @@ import View from './View.js';
 
 class ResultsView extends View {
     _parentEl = document.querySelector('.results');
+    _errorMessage = 'No recipe found for your query! Please try again ;)';
+    _message = '';
 
     _generateMarkup(result) {
         return this._data.map().join('');
