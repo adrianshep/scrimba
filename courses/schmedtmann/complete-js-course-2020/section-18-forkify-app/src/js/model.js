75b52dcdeb91f1248645192c7932037dfb1fab32
Adrian Shepelavey <adrianshepelavey@gmail.com>
2025-12-29 07:27:20 -0500

    pagination 1: by default, page should be that default

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/model.js
@@ -56,7 +56,7 @@ export const loadSearchResults = async function(query) {
     }
 };
 
-export const getSearchResultsPage = function(page) {
+export const getSearchResultsPage = function(page = state.search.page) {
     state.search.page = page;
 
     const start = (page - 1)
