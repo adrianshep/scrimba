0dabe9457584e08144c292c491efbb88d280430c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2026-02-24 07:13:10 -0500

    pagination 2: correct insert Page 1 markup for paginationView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/controller.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/controller.js
@@ -2037,3 +2037,15 @@ if(this._data.page === numPages && numPages > 1) {
   `;
 }
 */
+/*
+if(this._data.page === numPages && numPages > 1) {
+  return `
+    <button class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </button>
+  `;
+}
+*/
