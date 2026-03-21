a891e3f6a166b3599e2186e3eeadc9f1aa6e2f1a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2026-03-21 09:33:58 -0400

    pagination 2: last page goto in paginationView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/paginationView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/paginationView.js
@@ -27,14 +27,14 @@ class PaginationView extends View {
 
         if(curPage === numPages && numPages > 1) {
             return `
-                <button class="btn--inline pagination__btn--next">
-                    <span>Page ${curPage + 1}</span>
-                    <svg class="search__icon">
-                        <use href="${icons}#icon-arrow-right"></use>
+              <button data-goto="${curPage - 1}" class="btn--inline pagination___btn--prev">
+                <svg class="search__icon">
+                  <use href="${icons}#icon-arrow-left"></use>
                 </svg>
-                </button>
+                <span>Page ${curPage - 1}</span>
+              </button>
             `;
-        }
+          }
 
         if(curPage < numPages) {
             return `
