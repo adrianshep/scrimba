f5c4859bba413bc28f3e2d703838100f3861432d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2026-05-01 10:11:28 -0400

    updating recipe servings: closest method if no button in recipeView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
@@ -16,6 +16,7 @@ class RecipeView extends View {
     addHandlerUpdateServings(handler) {
       this._parentElement.addEventListener('click', function(e) {
         const btn = e.target.closest('.btn--tiny');
+        if (!btn) return;
   })
 }
 
