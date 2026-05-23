04f2daf4ad96cd455fa4323ee973183f5bbf13d0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2026-05-23 09:42:30 -0400

    updating recipe servings: add handler check in recipeView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
@@ -18,6 +18,7 @@ class RecipeView extends View {
         const btn = e.target.closest('.btn--update-servings');  
         if (!btn) return;
         const updateTo - +btn.dataset.updateTo;
+        if (updateTo > 0) handler(updateTo);
 
         handler();
   })
