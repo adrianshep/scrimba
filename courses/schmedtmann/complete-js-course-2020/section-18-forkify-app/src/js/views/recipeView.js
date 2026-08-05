449a3976f4af3b61ecec5b7b7358aaa644bc14b7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2026-08-05 08:55:28 -0400

    implementing bookmarks: render bookmark handler in recipe view

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/recipeView.js
@@ -165,7 +165,7 @@ addHandlerRender(handler) {
 
           <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}#icon-bookmark"></use>
+              <use href="${icons}#icon-bookmark${this._data.bookmarked ? '-fill' : '' }"></use>
             </svg>
           </button>
         </div>
