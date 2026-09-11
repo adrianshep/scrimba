c2360b8ecaa9d1b4cfc1a98534655f180d7510d8
Adrian Shepelavey <adrianshepelavey@gmail.com>
2026-09-11 08:08:16 -0400

    implement bookmarks 2: remove result markup in bookmarksView

--- courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/bookmarksView.js
+++ courses/schmedtmann/complete-js-course-2020/section-18-forkify-app/src/js/views/bookmarksView.js
@@ -8,26 +8,7 @@ class BookmarksView extends View {
 
     _generateMarkup(result) {
         return this._data.map().join('');
-    }
-
-    _generateMarkupPreview(result) {
-      const id = window.location.hash.slice(1);
-
-        return `
-            <li class="preview">
-                  <a class="preview__link ${
-                    result.id === id ? 'preview__link--active' : ''
-                  }" href="#${result.id}">
-                    <figure class="preview__fig">
-                      <img src="${result.image}" alt="${result.title}" />
-                    </figure>
-                    <div class="preview__data">
-                      <h4 class="preview__title">${result.title}</h4>
-                      <p class="preview__publisher">${result.publisher}</p>
-                  </a>
-                </li>
-          `;
-      }      
-}
+    } 
+  }
 
 export default new BookmarksView();
